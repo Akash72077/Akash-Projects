78815b801bb262dc8a1c13bcab572e43007d1fd7
Akash72077 <sai.sai72077@gmail.com>
2026-03-23 19:54:33 +0530

    Added 3 examples for conditional rendering and rendering using terinary operators

--- FrontEnd/React/Learning/React5/src/App.jsx
+++ FrontEnd/React/Learning/React5/src/App.jsx
@@ -1,9 +1,32 @@
-import React from 'react'
-import "./App.css"
+import React from "react";
+import "./App.css";
 const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+  //example-1
+  // const isLoggedIN=true;
 
-export default App
+  // let display;
+  // if(isLoggedIN==true){ // example for conditional rendering
+  //   display="Welcome to Hyderabad";
+  // }
+  // example-2
+  const isLoggedIN = true;
+
+  // let display;
+  // if(isLoggedIN==true){ // example for conditional rendering
+  //   display="Logout";
+  // }else{
+  //   display="Login in"
+  // }
+  // return (
+  //   // <button>{display}</button>
+
+  // )
+  // rendering using terinary operator
+  return (
+    <div>
+      {isLoggedIN ? <button>logout</button> : <button>Login</button>}
+    </div>
+  );
+};
+
+export default App;
