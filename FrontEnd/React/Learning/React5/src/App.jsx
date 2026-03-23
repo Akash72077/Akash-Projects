11b13ecd7b6cc6b438e1e873ebdbb414dd6705b2
Akash72077 <sai.sai72077@gmail.com>
2026-03-23 20:01:34 +0530

    Added one more example for rendering using AND operator

--- FrontEnd/React/Learning/React5/src/App.jsx
+++ FrontEnd/React/Learning/React5/src/App.jsx
@@ -3,7 +3,6 @@ import "./App.css";
 const App = () => {
   //example-1
   // const isLoggedIN=true;
-
   // let display;
   // if(isLoggedIN==true){ // example for conditional rendering
   //   display="Welcome to Hyderabad";
@@ -19,14 +18,18 @@ const App = () => {
   // }
   // return (
   //   // <button>{display}</button>
-
   // )
+  // example-3
   // rendering using terinary operator
-  return (
-    <div>
-      {isLoggedIN ? <button>logout</button> : <button>Login</button>}
-    </div>
-  );
+  // return (
+  //   <div>
+  //     {isLoggedIN ? <button>logout</button> : <button>Login</button>}
+  //   </div>
+  // );
+  // example-4
+  // rendering using and operator it will be used to render when the condition or value of the variable is true
+ // for this example the prarical example will be example 1
+  return <div>{isLoggedIN && <button>Logout</button>}</div>;
 };
 
 export default App;
