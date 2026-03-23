e77d3199504e75b9127024d6d1058887c7a31f86
Akash72077 <sai.sai72077@gmail.com>
2026-03-23 23:58:00 +0530

    made commet lines

--- FrontEnd/React/Learning/React67/src/App.jsx
+++ FrontEnd/React/Learning/React67/src/App.jsx
@@ -34,30 +34,30 @@
 // export default App;
 
 // On submit events for forms
-import React from "react";
-import { useState } from "react";
-const App = () => {
-  const [name, setName] = useState("");
-  const [submittedName, setSubmittedName]=useState();
-  function handleSumbit() {
-    event.preventDefault();
-    setSubmittedName(name);
-  }
-  return (
-    <div>
-      {/* when we submit form in react the page will refreshes automatically To avoid this refresh we have to perform another event that is event.preventDefault();
-       */}
-      <form onSubmit={handleSumbit}>
-        <input
-          type="text"
-          placeholder="Enter Your name:"
-          onChange={(e) => setName(e.target.value)}
-        />
-        <button type="submit">Submit</button>
-      </form>
-      {/*  displaying input data after submit */}
-      <h1>{submittedName}</h1>
-    </div>
-  );
-};
-export default App;
+// import React from "react";
+// import { useState } from "react";
+// const App = () => {
+//   const [name, setName] = useState("");
+//   const [submittedName, setSubmittedName] = useState();
+//   function handleSumbit() {
+//     event.preventDefault();
+//     setSubmittedName(name);
+//   }
+//   return (
+//     <div>
+//       {/* when we submit form in react the page will refreshes automatically To avoid this refresh we have to perform another event that is event.preventDefault();
+//        */}
+//       <form onSubmit={handleSumbit}>
+//         <input
+//           type="text"
+//           placeholder="Enter Your name:"
+//           onChange={(e) => setName(e.target.value)}
+//         />
+//         <button type="submit">Submit</button>
+//       </form>
+//       {/*  displaying input data after submit */}
+//       <h1>{submittedName}</h1>
+//     </div>
+//   );
+// };
+// export default App;
