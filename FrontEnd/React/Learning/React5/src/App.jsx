0a5c0845fcac4c0730feecdc8987646657b121f4
Akash72077 <sai.sai72077@gmail.com>
2026-03-23 20:51:50 +0530

    Completed a mini project using conditional rendering to shpw/hide passwprd

--- FrontEnd/React/Learning/React5/src/App.jsx
+++ FrontEnd/React/Learning/React5/src/App.jsx
@@ -30,20 +30,34 @@ const App = () => {
   // for this example the prarical example will be example 1
   // return <div>{isLoggedIN && <button>Logout</button>}
   // </div>;
-// project-1
+  // project-1
   const [show, setShow] = useState(false);
   // const handleButton = () => {
-    // if (show === true) {
-    //   setShow(false);
-    // } else {
-    //   setShow(true);
-    // }
-    // setShow(!show); // insted of using if and else we can use (!show) if the input is true then it will become false if false it will become true
+  // if (show === true) {
+  //   setShow(false);
+  // } else {
+  //   setShow(true);
+  // }
+  // setShow(!show); // insted of using if and else we can use (!show) if the input is true then it will become false if false it will become true
   // };
+  //this return function is for project 1
+  // return (
+  //   <div>
+  //     <button onClick={()=>setShow(!show)}>{show ? "Hide" : "Show"}</button>
+  //     {show && <p>This is a Secret Message!</p>}
+  //   </div>
+  // );
+  // project -2 show / hide app password
+  const [showPassword, setShowPassword] = useState(false);
   return (
     <div>
-      <button onClick={()=>setShow(!show)}>{show ? "Hide" : "Show"}</button>
-      {show && <p>This is a Secret Message!</p>}
+      <input
+        type={showPassword ? "text" : "password"}
+        placeholder="Enter Your Password"
+      />
+      <button onClick={() => setShowPassword(!showPassword)}>
+        {showPassword ? "Hide" : "Show"}
+      </button>
     </div>
   );
 };
