56127a142f529b5670258d38d7260ae9a0c167ed
Akash72077 <sai.sai72077@gmail.com>
2026-03-23 21:40:09 +0530

    commented all lines

--- FrontEnd/React/Learning/React6/src/App.jsx
+++ FrontEnd/React/Learning/React6/src/App.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import "./App.css";
-const App = () => {
-  function clickButton() {
-    alert("button clicked!");
-  }
-  return (
-    <div>
-      <button onClick={clickButton}>Click</button>
-    </div>
-  );
-};
+// import React from "react";
+// import "./App.css";
+// const App = () => {
+//   function clickButton(name) {
+//     alert(`Hello ${name}`);
+//   }
+//   return (
+//     <div>
+//       <button onClick={()=>clickButton("Akash")}>Click</button>
+//     </div>
+//   );
+// };
 
-export default App;
+// export default App;
