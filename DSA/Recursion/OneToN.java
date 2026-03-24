e585d5d35c8588403e2e5921b2730182daa177d7
Akash72077 <sai.sai72077@gmail.com>
2026-03-24 15:53:47 +0530

    Deleted is sorted file and written code to print the numbers from 1 to n

--- DSA/Recursion/OneToN.java
+++ DSA/Recursion/OneToN.java
@@ -1,14 +1,19 @@
+import java.util.Scanner;
 public class OneToN {
-
-    public static void print(int n){
-        if(n==0){
-            return ;
+    static void printIncreasing(int n){
+        if(n==1){
+            System.out.println(n);
+            return;
         }
-             print(n-1);
-            System.out.print(n+" ");
+        printIncreasing(n-1);
+        System.out.println(n);
+
     }
-    public static void main(String[] args) {
-        int n=5;
-        print(n);
+       public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        printIncreasing(n);
     }
 }
+
+  
