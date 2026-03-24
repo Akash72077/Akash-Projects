2ff839725944081f2352598124e680b1b35cd00b
Akash72077 <sai.sai72077@gmail.com>
2026-03-24 19:49:37 +0530

    .

--- Zerodha/frontend/src/landing_page/home/Pricing.js
+++ Zerodha/frontend/src/landing_page/home/Pricing.js
@@ -4,7 +4,7 @@ function Pricing() {
     <div className="container">
       <div className="row">
         <div className="col-4">
-            
+          <h1 className="mb-3">Unbeatable Pricing</h1>
         </div>
         <div className="col-2"></div>
         <div className="col-4"></div>
