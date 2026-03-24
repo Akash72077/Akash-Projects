3ca2b61bc436bf5e89533b75ff02646eff8da733
Akash72077 <sai.sai72077@gmail.com>
2026-03-24 19:52:39 +0530

    added paragraph

--- Zerodha/frontend/src/landing_page/home/Pricing.js
+++ Zerodha/frontend/src/landing_page/home/Pricing.js
@@ -5,6 +5,14 @@ function Pricing() {
       <div className="row">
         <div className="col-4">
           <h1 className="mb-3">Unbeatable Pricing</h1>
+          <p>
+            We poineered the concept of discount broking and price transparency
+            in India. Flat frees and no hidden charges.
+          </p>
+          <a href="" style={{ textDecoration: "none" }}>
+            See Pricing{" "}
+            <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+          </a>
         </div>
         <div className="col-2"></div>
         <div className="col-4"></div>
