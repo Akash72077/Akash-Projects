efc061931a1c9a28297544c33acfa0df9d6358cf
Akash72077 <sai.sai72077@gmail.com>
2026-03-24 16:05:26 +0530

    Completed Code to print n to 1

--- DSA/Recursion/NToOne.java
+++ DSA/Recursion/NToOne.java
@@ -1,4 +1,13 @@
 public class NToOne {
+    static void printDecreasing(int n){
+        if(n<=1){
+            System.out.println(n);
+            return;
+        }
+        System.out.println(n);
+        printDecreasing(n-1);
+    }
     public static void main(String[] args) {
+        printDecreasing(6);
     }
 }
