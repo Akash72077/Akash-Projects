de536525e37ec4685204b0160e3f50f1d87ce427
Akash72077 <sai.sai72077@gmail.com>
2026-03-25 19:06:28 +0530

    added comment line

--- DSA/1D & 2D Arrays/DSA.java
+++ DSA/1D & 2D Arrays/DSA.java
@@ -1,11 +1,10 @@
 import java.util.Scanner;
-
 public class DSA {
-    
     static int tripletSum(int arr[], int target){
         //program to count the total number of triplets whouse sum is equal to the given value of X
         int ans=0;
         int n=arr.length;
+        //time complexity O(n^3)
          for(int i=0; i<n; i++){
             for(int j=i+1; j<n; j++){
                 for(int k=j+1; k<n; k++){
@@ -15,13 +14,13 @@ public class DSA {
                 }
             }
         }
-
         return ans;
     }
     static int pairSum(int arr[], int target){
         //program to find the total number of pairs in the arrays whose sum is equal to given target
         int n=arr.length;
         int ans=0;
+        //time complexity O(n^2)
         for(int i=0; i<n; i++){
             for(int j=i+1; j<n; j++){
                 if(arr[i]+arr[j]==target){
@@ -42,7 +41,6 @@ public class DSA {
         }
         System.out.println("Enter target sum: ");
         int target=sc.nextInt();
-
         System.out.println("The no of Possible ways for pair sum is: "+pairSum(arr, target));
         System.out.println("The no of Possible ways for Triplet sum is: "+tripletSum(arr, target));
         sc.close();
