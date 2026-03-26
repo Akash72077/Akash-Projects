a79dc1eeccfaf18312d0bc9d71ec12a2fbe56bae
Akash72077 <sai.sai72077@gmail.com>
2026-03-26 23:30:40 +0530

    added loop

--- DSA/1D & 2D Arrays/DSA.java
+++ DSA/1D & 2D Arrays/DSA.java
@@ -1,37 +1,37 @@
 import java.util.Scanner;
 public class DSA {
     
-    static int tripletSum(int arr[], int target){
-        //program to count the total number of triplets whouse sum is equal to the given value of X
-        int ans=0;
-        int n=arr.length;
-        //time complexity O(n^3)
-         for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                for(int k=j+1; k<n; k++){
-                    if(arr[i]+arr[j]+arr[k]==target){
-                    ans++;
-                }
-                }
-            }
-        }
-        return ans;
-    }
-    static int pairSum(int arr[], int target){
-        //program to find the total number of pairs in the arrays whose sum is equal to given target
-        int n=arr.length;
-        int ans=0;
-        //time complexity O(n^2)
-        for(int i=0; i<n; i++){
-            for(int j=i+1; j<n; j++){
-                if(arr[i]+arr[j]==target){
-                    ans++;
+        static int tripletSum(int arr[], int target){
+            //program to count the total number of triplets whouse sum is equal to the given value of X
+            int ans=0;
+            int n=arr.length;
+            //time complexity O(n^3)
+            for(int i=0; i<n; i++){
+                for(int j=i+1; j<n; j++){
+                    for(int k=j+1; k<n; k++){
+                        if(arr[i]+arr[j]+arr[k]==target){
+                        ans++;
+                    }
+                    }
                 }
             }
+            return ans;
         }
-        return ans;
-    }
-    static int UniqueBitWise(int arr[]){
+        static int pairSum(int arr[], int target){
+            //program to find the total number of pairs in the arrays whose sum is equal to given target
+            int n=arr.length;
+            int ans=0;
+            //time complexity O(n^2)
+            for(int i=0; i<n; i++){
+                for(int j=i+1; j<n; j++){
+                    if(arr[i]+arr[j]==target){
+                        ans++;
+                    }
+                }
+            }
+            return ans;
+        }
+        static int UniqueBitWise(int arr[]){
             // Find the unique nimber in a given arrat where all elements are being repeated twice with one value being unique
             // the elements are postive
             int n=arr.length;
@@ -96,6 +96,14 @@ public class DSA {
             }
             return -1;         
         }
+        static void swap(int a, int b){
+            System.out.println("Original values before swap: ");
+            System.out.println("a: "+a);
+            System.out.println("b: "+b);
+            System.out.println("After swaping values: ");
+            System.out.println("a: "+a);
+            System.out.println("b: "+b);
+        }    
         public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         System.out.println("Enter the size of arr: ");
@@ -110,7 +118,7 @@ public class DSA {
        // System.out.println("The no of Possible ways for pair sum is: "+pairSum(arr, target));
       //  System.out.println("The no of Possible ways for Triplet sum is: "+tripletSum(arr, target));
      // System.out.println(UniqueBitWise(arr));
-    //   System.out.println(unique(arr));
+    //System.out.println(unique(arr));
         // System.out.println(SecondLargest(arr));
         // System.out.println(findMax(arr));
         System.out.println("First Repeating Number: "+firstRepeatingNumber(arr));
