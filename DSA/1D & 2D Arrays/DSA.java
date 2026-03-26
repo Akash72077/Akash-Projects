4623811ccf43e3fd4bd2e081256be2c1a146714e
Akash72077 <sai.sai72077@gmail.com>
2026-03-26 23:31:34 +0530

    cmt

--- DSA/1D & 2D Arrays/DSA.java
+++ DSA/1D & 2D Arrays/DSA.java
@@ -108,7 +108,7 @@ public class DSA {
         Scanner sc= new Scanner(System.in);
         System.out.println("Enter the size of arr: ");
         int n=sc.nextInt();
-        int arr[]=new int[n];
+        int arr[]=new int[n]; //declaring array
         System.out.println("Enter "+n+" Elements: ");
         for(int i=0; i<n; i++){
            arr[i]=sc.nextInt();
