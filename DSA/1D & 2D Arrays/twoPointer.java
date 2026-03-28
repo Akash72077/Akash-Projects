982c39af598e6724a0da649050ec201d9b7449b1
Akash72077 <sai.sai72077@gmail.com>
2026-03-28 23:59:42 +0530

    created array

--- DSA/1D & 2D Arrays/twoPointer.java
+++ DSA/1D & 2D Arrays/twoPointer.java
@@ -3,6 +3,7 @@ import java.util.Scanner;
 public class twoPointer {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
-        
+        int n=sc.nextInt();
+        int arr[] = new int[n];
     }
 }
