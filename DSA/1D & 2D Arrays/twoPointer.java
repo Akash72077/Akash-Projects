1d3b897c50c1cf66d4aba2a2637af1177d6d00cf
Akash72077 <sai.sai72077@gmail.com>
2026-03-29 00:20:31 +0530

    code completed to sort an array when the input is 0's and 1's

--- DSA/1D & 2D Arrays/twoPointer.java
+++ DSA/1D & 2D Arrays/twoPointer.java
@@ -1,9 +1,37 @@
 import java.util.Scanner;
-
 public class twoPointer {
+    static void printArr(int arr[]){
+        int n=arr.length;
+        for(int i=0; i<n; i++){
+            System.out.print(arr[i]+" ");
+        }
+    }
+    static void sortArrayZerosOnes(int arr[]){
+        int n=arr.length;
+        int countZero=0;
+        for(int i=0; i<n; i++){
+            if(arr[i]==0){
+                countZero++;
+            }
+        }
+        for(int i=0; i<countZero; i++){
+            arr[i]=0;
+        }
+        for(int i=countZero; i<n; i++){
+            arr[i]=1;
+        }
+        printArr(arr);
+    }
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the Size of an Array: ");
         int n=sc.nextInt();
         int arr[] = new int[n];
+        System.out.println("Enter "+n+" Elements: ");
+         for(int i=0; i<n; i++){
+            arr[i]=sc.nextInt();
+        }
+        sortArrayZerosOnes(arr);
+        sc.close();
     }
 }
