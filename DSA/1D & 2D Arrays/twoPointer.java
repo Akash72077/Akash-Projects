529b9841ddbebc1f3d0dcbc01be535f73a0b061e
Akash72077 <sai.sai72077@gmail.com>
2026-03-29 00:52:40 +0530

    completed code for efficeint approach

--- DSA/1D & 2D Arrays/twoPointer.java
+++ DSA/1D & 2D Arrays/twoPointer.java
@@ -30,6 +30,26 @@ public class twoPointer {
         }
         printArr(arr);
     }
+    // efficient approch completes sort in one iteration
+    static void sortArrayZerosOnesEfficient(int arr[]){
+         int n=arr.length;
+         int i=0;
+         int j=n-1;
+         while(i<j){
+            if(arr[i]==1&&arr[j]==0){
+                swap(arr, i, j);
+                i++;
+                j--;
+            }
+            if(arr[i]==0){
+                i++;
+            }
+            if(arr[j]==1){
+                j--;
+            }
+         }
+         printArr(arr);
+    }
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         System.out.println("Enter the Size of an Array: ");
@@ -39,7 +59,8 @@ public class twoPointer {
          for(int i=0; i<n; i++){
             arr[i]=sc.nextInt();
         }
-        sortArrayZerosOnes(arr);
+        // sortArrayZerosOnes(arr);
+        sortArrayZerosOnesEfficient(arr);
         sc.close();
     }
 }
