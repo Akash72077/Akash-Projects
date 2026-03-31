99124258fffba1e67bd9fb479d10b1982f0d4c0f
Akash72077 <sai.sai72077@gmail.com>
2026-04-01 00:57:52 +0530

    added question

--- DSA/Patterns/PrefixSum.java
+++ DSA/Patterns/PrefixSum.java
@@ -1,14 +1,13 @@
 import java.util.Scanner;
-
 public class PrefixSum {
+    // given an array of 'a', return the prefix sum/running sum in the same array
     static void printArr(int arr[], int n){
-        
          for(int i=1; i<=n; i++){
               System.out.print(arr[i]+" ");
         }
     }
     static void prefixSum(int arr[], int n){
-        
+        // For a given array of integers of size n, answer q queries to print the sum of values in a given range of indices from l to r
        for(int i=2; i<=n; i++){
                arr[i]=arr[i]+arr[i-1];
         }
