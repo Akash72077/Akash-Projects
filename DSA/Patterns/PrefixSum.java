e4b9c70e8391b9a8d1c5537f2d9efa61a22dd20f
Akash72077 <sai.sai72077@gmail.com>
2026-04-01 23:46:08 +0530

    started

--- DSA/Patterns/PrefixSum.java
+++ DSA/Patterns/PrefixSum.java
@@ -1,5 +1,13 @@
 import java.util.Scanner;
 public class PrefixSum {
+    static void checkSum(int arr[], int n){
+// Check if we can partition the array into two subarrays with equal sum. More formally, check that the perifx sum of parts of the arrays is equal to the suffix sum of rest of the array.
+        for(int i=1; i<=n; i++){
+               arr[i]=arr[i]+arr[i-1];
+        }
+        
+        
+    }
     // given an array of 'a', return the prefix sum/running sum in the same array
     static void printArr(int arr[], int n){
          for(int i=1; i<=n; i++){
@@ -17,12 +25,13 @@ public class PrefixSum {
         Scanner sc= new Scanner(System.in);
         System.out.println("Enter the size of an array: ");
         int n=sc.nextInt();
-        int arr[]= new int[n+1];
+        int arr[]= new int[n];// for 0 based indexing
+        //  int arr1[]= new int[n+1];// for 1 based indexing
         System.out.println("Enter "+n+" Elements: ");
-        for(int i=1; i<=n; i++){
-            arr[i]=sc.nextInt();    
-        }
-        prefixSum(arr,n);
+        // for(int i=1; i<=n; i++){
+        //     arr1[i]=sc.nextInt();    
+        // }
+        // prefixSum(arr1,n);
         sc.close();
     }
 }
