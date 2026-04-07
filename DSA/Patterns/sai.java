761fb049a285022ab184b9197f3b9e24ee2e595a
Akash72077 <sai.sai72077@gmail.com>
2026-04-07 23:52:34 +0530

    s

--- DSA/Patterns/sai.java
+++ DSA/Patterns/sai.java
@@ -2,7 +2,7 @@ class sai{
     public static void main(String[] args) {
          int arr[] = new int[5];
         arr[0]=1;
-        arr[1]=2
+        arr[1]=2;
 
         arr[2]=3;
         arr[3]=4;
