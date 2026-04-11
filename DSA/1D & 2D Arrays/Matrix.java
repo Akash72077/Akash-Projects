1c2d67b26920c2ba8337333c002988135607fd31
Akash72077 <sai.sai72077@gmail.com>
2026-04-11 23:55:23 +0530

    added functionality

--- DSA/1D & 2D Arrays/Matrix.java
+++ DSA/1D & 2D Arrays/Matrix.java
@@ -1,6 +1,24 @@
 import java.util.Scanner;
 
 public class Matrix {
+    static void printArr(int arr[][]){
+        for(int i=0; i<arr.length;i++){
+            for(int j=0; j<arr[i].length; j++){
+                System.out.print(arr[i][j]+" ");
+            }
+            System.out.println();
+        }
+    }
+    static void addition(int mat1[][], int mat2[][], int m , int n){
+        int result[][]= new int[m][n];
+        for(int i=0; i<m;i++){
+            for(int j=0; j<n; j++){
+                result[i][j]=mat1[i][j]+mat2[i][j];
+            }
+        }
+        System.out.println("The addition matrix: ");
+        printArr(result);
+    }
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         //addition of matrix
@@ -9,7 +27,7 @@ public class Matrix {
         int n=sc.nextInt();
         int mat1[][]= new int[m][n];
         int mat2[][]= new int[m][n];
-        int result[][]= new int[m][n];
+        
         System.out.println("Enter the values for Matrix-1: ");
         for(int i=0; i<m;i++){
             for(int j=0; j<n; j++){
@@ -22,20 +40,7 @@ public class Matrix {
                 mat2[i][j]=sc.nextInt();
             }
         }
-        // code to calculate and store in result array
-        for(int i=0; i<m;i++){
-            for(int j=0; j<n; j++){
-                result[i][j]=mat1[i][j]+mat2[i][j];
-            }
-        }
-        //code to print the matrix 
-        System.out.println("The addition matrix: ");
-         for(int i=0; i<m;i++){
-            for(int j=0; j<n; j++){
-                System.out.print(result[i][j]+" ");
-            }
-            System.out.println();
-        }
+        addition(mat1, mat2, m, n);
         sc.close();
     }
 }
