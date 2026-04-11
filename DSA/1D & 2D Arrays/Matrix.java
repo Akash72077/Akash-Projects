9b55972e5e5c666e797ada973d478fcd999ffcd5
Akash72077 <sai.sai72077@gmail.com>
2026-04-11 23:40:41 +0530

    taken input for mat1

--- DSA/1D & 2D Arrays/Matrix.java
+++ DSA/1D & 2D Arrays/Matrix.java
@@ -8,5 +8,10 @@ public class Matrix {
         int m=sc.nextInt();
         int n=sc.nextInt();
         int mat1[][]= new int[m][n];
+        for(int i=0; i<m;i++){
+            for(int j=0; j<n; j++){
+                mat1[i][j]=sc.nextInt();
+            }
+        }
     }
 }
