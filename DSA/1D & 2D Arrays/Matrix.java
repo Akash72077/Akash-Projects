a910a241b1e1d0fc4d4c569d01b0ae18587cf7f1
Akash72077 <sai.sai72077@gmail.com>
2026-04-11 23:39:14 +0530

    created a matrix and taken size as input

--- DSA/1D & 2D Arrays/Matrix.java
+++ DSA/1D & 2D Arrays/Matrix.java
@@ -1,4 +1,12 @@
+import java.util.Scanner;
+
 public class Matrix {
     public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        //addition of matrix
+        System.out.println("Enter the no of rows & colums of matrix: ");
+        int m=sc.nextInt();
+        int n=sc.nextInt();
+        int mat1[][]= new int[m][n];
     }
 }
