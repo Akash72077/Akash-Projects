8c26708416b15a6d81127d7cd09d8504e38117b9
Akash72077 <sai.sai72077@gmail.com>
2026-04-11 23:41:35 +0530

    added mat2

--- DSA/1D & 2D Arrays/Matrix.java
+++ DSA/1D & 2D Arrays/Matrix.java
@@ -8,9 +8,15 @@ public class Matrix {
         int m=sc.nextInt();
         int n=sc.nextInt();
         int mat1[][]= new int[m][n];
+        int mat2[][]= new int[m][n];
         for(int i=0; i<m;i++){
             for(int j=0; j<n; j++){
                 mat1[i][j]=sc.nextInt();
+            }
+        }
+        for(int i=0; i<m;i++){
+            for(int j=0; j<n; j++){
+                mat2[i][j]=sc.nextInt();
             }
         }
     }
