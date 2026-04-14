49900893395d351bf8de4b69647c2450f8d06863
Akash72077 <sai.sai72077@gmail.com>
2026-04-14 23:35:18 +0530

    added cmt

--- Python/Button2.py
+++ Python/Button2.py
@@ -1,11 +1,11 @@
 import tkinter # step 1
 from tkinter import messagebox
-# Created GUI window
+# Created GUI window step 2
 window = tkinter.Tk()
 # rename
 window.title("GUI")
 def callback():
-    messagebox.showinfo("Title", "Hello CMREC")
-B=tkinter.Button(window, text="click", command= callback)
+    messagebox.showinfo("Title", "Hello CMREC") 
+B=tkinter.Button(window, text="click", command= callback)#Create layout
 B.pack()
 window.mainloop()
