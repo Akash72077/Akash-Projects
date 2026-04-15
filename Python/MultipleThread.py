74b0efcdcb943b3c71e8b40157787ebc7c2e65c1
Akash72077 <sai.sai72077@gmail.com>
2026-04-15 23:59:23 +0530

    done

--- Python/MultipleThread.py
+++ Python/MultipleThread.py
@@ -1,11 +1,9 @@
 # import threading
 # def task():
 #     print("Thread is running")
-
 # # Create threads
 # t1 = threading.Thread(target=task)
 # t2 = threading.Thread(target=task)
-
 # # Start threads
 # t1.start()
 # t2.start()
@@ -14,7 +12,6 @@
 # t2.join()
 # print("Main program finished")
 import threading
-
 # Function to print even numbers
 def print_even():
     for i in range(1, 21):
