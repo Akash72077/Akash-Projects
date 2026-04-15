829cab5a6034e8037197895142d8c3c46d6935cc
Akash72077 <sai.sai72077@gmail.com>
2026-04-15 23:55:41 +0530

    b

--- Python/MultipleThread.py
+++ Python/MultipleThread.py
@@ -1,18 +1,46 @@
+# import threading
+
+# def task():
+#     print("Thread is running")
+
+# # Create threads
+# t1 = threading.Thread(target=task)
+# t2 = threading.Thread(target=task)
+
+# # Start threads
+# t1.start()
+# t2.start()
+
+# # Wait for threads to finish
+# t1.join()
+# t2.join()
+
+# print("Main program finished")
+
 import threading
 
-def task():
-    print("Thread is running")
+# Function to print even numbers
+def print_even():
+    for i in range(1, 21):
+        if i % 2 == 0:
+            print("Even:", i)
+
+# Function to print odd numbers
+def print_odd():
+    for i in range(1, 21):
+        if i % 2 != 0:
+            print("Odd:", i)
 
 # Create threads
-t1 = threading.Thread(target=task)
-t2 = threading.Thread(target=task)
+t1 = threading.Thread(target=print_even)
+t2 = threading.Thread(target=print_odd)
 
 # Start threads
 t1.start()
 t2.start()
 
-# Wait for threads to finish
+# Wait for both threads to finish
 t1.join()
 t2.join()
 
-print("Main program finished")
+print("Program finished")
