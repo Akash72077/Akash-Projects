e86d56941c84d2f68cdbeeebcdd733bc6b38f277
Akash72077 <sai.sai72077@gmail.com>
2026-04-15 23:58:03 +0530

    b

--- Python/MultipleThread.py
+++ Python/MultipleThread.py
@@ -24,13 +24,11 @@ def print_even():
     for i in range(1, 21):
         if i % 2 == 0:
             print("Even:", i)
-
 # Function to print odd numbers
 def print_odd():
     for i in range(1, 21):
         if i % 2 != 0:
             print("Odd:", i)
-
 # Create threads
 t1 = threading.Thread(target=print_even)
 t2 = threading.Thread(target=print_odd)
