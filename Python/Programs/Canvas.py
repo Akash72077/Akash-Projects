780836cc693991fb52e7dabd392a3a596fbe39d2
Akash72077 <sai.sai72077@gmail.com>
2026-04-25 23:58:11 +0530

    run

--- Python/Programs/Canvas.py
+++ Python/Programs/Canvas.py
@@ -1,7 +1,9 @@
-import tkinter
-window=tkinter.Tk()
+import tkinter  # step 1
+window=tkinter.Tk() #window created 
+#renamed
+window.title("Canvaas")
 c=tkinter.Canvas(window, bg="blue", height=250,width=250)
 coord= 10, 50 , 240 , 210
-arc= c.create_arc(coord,start=0, extent=150, fill="red")
-c.pack()
-window.mainloop()
+arc= c.create_arc(coord,start=0, extent=150, fill="red") #create widget or componnent 
+c.pack() #link component
+window.mainloop() #run main event loop
