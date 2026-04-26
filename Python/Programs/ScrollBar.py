3f033d4650362380c03a3b2e28c204998e7d3a3e
Akash72077 <sai.sai72077@gmail.com>
2026-04-26 23:49:34 +0530

    radio button

--- Python/Programs/ScrollBar.py
+++ Python/Programs/ScrollBar.py
@@ -1,7 +1,6 @@
 import tkinter
 from tkinter import *
 window = tkinter.Tk()
-
 sb=tkinter.Scrollbar(window)
 sb.pack(side=RIGHT, fill=Y)
 mylist=tkinter.Listbox(window, yscrollcommand=sb.set)
@@ -9,6 +8,4 @@ for line in range(51):
     mylist.insert(END, "This is line number " +str(line))
 mylist.pack(side=LEFT, fill=BOTH)
 sb.config(command=mylist.yview)
-
-
 window.mainloop()
