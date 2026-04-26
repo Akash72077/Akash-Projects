e269e2d1f0226a4defbc8a3a1b46559fceb156f5
Akash72077 <sai.sai72077@gmail.com>
2026-04-26 23:45:24 +0530

    complted

--- Python/Programs/Thread.py
+++ Python/Programs/Thread.py
@@ -1,18 +1,13 @@
 import threading
-
 def task():
     print("Thread is running")
-
 # Create threads
 t1 = threading.Thread(target=task)
 t2 = threading.Thread(target=task)
-
 # Start threads
 t1.start()
 t2.start()
-
 # Wait for threads to finish
 t1.join()
 t2.join()
-
 print("Main program finished")
