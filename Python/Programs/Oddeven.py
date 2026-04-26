8ce769b071b3c6bb1c3826cd314b6c46f5c230ca
Akash72077 <sai.sai72077@gmail.com>
2026-04-26 23:48:56 +0530

    renamed

--- Python/Programs/Oddeven.py
+++ Python/Programs/Oddeven.py
@@ -1,16 +1,4 @@
-# import threading
-# def task():
-#     print("Thread is running")
-# # Create threads
-# t1 = threading.Thread(target=task)
-# t2 = threading.Thread(target=task)
-# # Start threads
-# t1.start()
-# t2.start()
-# # Wait for threads to finish
-# t1.join()
-# t2.join()
-# print("Main program finished")
+
 import threading
 # Function to print even numbers
 def print_even():
