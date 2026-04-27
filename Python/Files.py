0d46298a70130ef0cb67807b4ea36479decb950d
Akash72077 <sai.sai72077@gmail.com>
2026-04-27 19:25:01 +0530

    code written for readline methods for both with and without size

--- Python/Files.py
+++ Python/Files.py
@@ -5,5 +5,11 @@ fo=open("file.txt", "r")
 #input methods 
 # data= fo.read(5)
 # print(data) # reads 5 lines if length not mentioned it will read upto end of the file
-data1=fo.read()
-print(data1)
+# data1=fo.read()
+# print(data1)
+#if size not given reads a single line 
+rd=fo.readline()
+print(rd)
+#if size given read size number of characters 
+rd=fo.readline(5)
+print(rd)
