89614962f9339cf106c3112ce6f64520aa37cf5e
Akash72077 <sai.sai72077@gmail.com>
2026-04-27 19:20:40 +0530

    read method

---
+++ Python/Files.py
@@ -0,0 +1,7 @@
+fo=open("file.txt", "r")
+# print(fo.name)
+# print(fo.mode)
+# print(fo.closed)
+#input methods 
+data= fo.read(5)
+print(data) # reads 5 lines if length not mentioned it will read upto end of the file
