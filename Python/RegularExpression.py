0172bbac35739842fe2e298d34db9e464bde6890
Akash72077 <sai.sai72077@gmail.com>
2026-04-27 23:45:27 +0530

    code for split

--- Python/RegularExpression.py
+++ Python/RegularExpression.py
@@ -6,7 +6,11 @@
 # a="my name is Akash, My age is 19"
 # b=re.search("name",a)
 # print(b)
-import re
-a="my name is Akash, my age is 19"
-b=re.findall("my",a)
-print(b)
+# import re
+# a="my name is Akash, my age is 19"
+# b=re.findall("my",a)
+# print(b)
+# import re
+# a="my name is Akash, My age is 19"
+# b=re.split("my",a)
+# print(b)
