ccbc0460a93eedaf8793828bd660f64a05864673
Akash <sai.sai72077@gmail.com>
2026-04-29 23:51:43 +0530

    Delete Python/Programs/Entry2.py

--- Python/Programs/Entry2.py
+++
@@ -1,8 +0,0 @@
-import tkinter
-from tkinter import * 
-window=tkinter.Tk()
-l=tkinter.Label(window, text="User Name: ")
-l.pack(side=LEFT)
-e=tkinter.Entry(window)
-e.pack(side=RIGHT)
-window.mainloop()
