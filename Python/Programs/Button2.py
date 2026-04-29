789268ec060687c971086e0dcac6b7250ec07daf
Akash <sai.sai72077@gmail.com>
2026-04-29 23:49:44 +0530

    Delete Python/Programs/Button2.py

--- Python/Programs/Button2.py
+++
@@ -1,9 +0,0 @@
-import tkinter
-from tkinter import messagebox
-window = tkinter.Tk()
-window.title("GUI")
-def callback():
-    messagebox.showinfo("Title", "Hello CMREC")
-B=tkinter.Button(window, text="click", command= callback)
-B.pack()
-window.mainloop()
