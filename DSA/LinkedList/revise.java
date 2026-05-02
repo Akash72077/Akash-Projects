a12210fa5f29ab36c5b3f8bfdb0301c816f99ebe
Akash72077 <sai.sai72077@gmail.com>
2026-05-02 21:08:25 +0530

    created a Linked List

--- DSA/LinkedList/revise.java
+++ DSA/LinkedList/revise.java
@@ -1,3 +1,29 @@
 public class revise {
-    
+    public static class Node{
+        int data; 
+        Node next;
+        Node(int data){
+            this.data=data;
+        }
+    }
+    public static void main(String[] args) {
+        Node a= new Node(0);
+        Node b= new Node(5);
+        Node c= new Node(4);
+        Node d= new Node(9);
+        Node e= new Node(12);
+        Node f= new Node(24);
+        System.out.println(a.data);
+        System.out.println(b.data);
+        System.out.println(c.data);
+        System.out.println(d.data);
+        System.out.println(e.data);
+        System.out.println(f.data); 
+        a.next=b;
+        b.next=c;
+        c.next=d;
+        d.next=e;
+        e.next=f;
+        f.next=null;
+    }
 }
