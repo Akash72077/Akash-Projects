1a1d7ef4a0e847d7bc09ec8f8dc3e6b274f9584c
Akash72077 <sai.sai72077@gmail.com>
2026-05-06 21:03:04 +0530

    function created to take input and display the elements

--- DSA/1D & 2D Arrays/MergeSortedArray.java
+++ DSA/1D & 2D Arrays/MergeSortedArray.java
@@ -1,6 +1,19 @@
 import java.util.Scanner;
 public class MergeSortedArray {
-    
+    static void display(int nums1[], int nums2[]){
+        int m=nums1.length;
+        System.out.println("Elements in nums 1: ");
+        for(int i=0; i<m; i++){
+            System.out.print(nums1[i]+" ");
+        }
+        System.out.println();
+        int n=nums2.length;
+        System.out.println("Elements in nums 2: ");
+        for(int i=0; i<n; i++){
+            System.out.print(nums2[i]+" ");
+        }
+        System.out.println();
+    }
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
         System.out.println("Enter the size of array1: ");
@@ -17,7 +30,7 @@ public class MergeSortedArray {
         for(int i=0; i<n; i++){
             nums2[i]=sc.nextInt();
         }
-        
+        display(nums1 , nums2);
         sc.close();
     }
 }
