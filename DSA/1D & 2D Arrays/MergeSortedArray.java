c430bc4367c9eb3dc69806a067872e9f9bc9fd85
Akash72077 <sai.sai72077@gmail.com>
2026-05-06 20:57:28 +0530

    written a broiler plate code

--- DSA/1D & 2D Arrays/MergeSortedArray.java
+++ DSA/1D & 2D Arrays/MergeSortedArray.java
@@ -1,5 +1,23 @@
+import java.util.Scanner;
 public class MergeSortedArray {
+    
     public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the size of array1: ");
+        int m=sc.nextInt();
+        System.out.println("Enter the size of array 2:");
+        int n=sc.nextInt();
+        int nums1[]= new int[m+n];
+        int nums2[]=new int[n];
+        System.out.println("Enter "+m+" Elements for array1: ");
+        for(int i=0; i<m; i++){
+            nums1[i]=sc.nextInt();
+        }
+        System.out.println("Enter "+n+" Elements for array2: ");
+        for(int i=0; i<n; i++){
+            nums2[i]=sc.nextInt();
+        }
         
+        sc.close();
     }
 }
