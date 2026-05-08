47f7f74d7558df0f3ab11800ab789a8435ec573b
Akash72077 <sai.sai72077@gmail.com>
2026-05-08 18:34:20 +0530

    written code to set ith bit to 1

---
+++ DSA/JavaBits/set_ith_bit.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class set_ith_bit {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n=sc.nextInt();
+        System.out.println("Enter i value (postion): ");
+        int i=sc.nextInt();
+        int bitmask=1<<i;
+        int result=n| bitmask;
+        System.out.println(result);
+        sc.close();
+    }
+}
