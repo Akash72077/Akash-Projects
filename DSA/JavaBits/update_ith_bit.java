2bb599f3b446abb7f7e82fad12051725407ba007
Akash72077 <sai.sai72077@gmail.com>
2026-05-08 21:27:15 +0530

    Written code to update ith bit

---
+++ DSA/JavaBits/update_ith_bit.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class update_ith_bit {
+    static int set_ith_bit(int n, int i){
+        int bitmask=1<<i;
+        return bitmask | n;
+    }
+    static int Clear_ith_bit(int n, int i){
+       int bitmask=1<<i;
+        return ~bitmask & n;
+    }
+    static int Update_ith_bit(int n, int i, int newBit){
+        if(newBit==0){
+            return Clear_ith_bit(n, i);
+        }
+        else{
+            return set_ith_bit(n, i);
+        }
+    }
+    public static void main(String[] args) {
+          Scanner sc= new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n=sc.nextInt();
+        System.out.println("Enter postion : ");
+        int i=sc.nextInt();
+        System.out.println(Update_ith_bit(n, i, 0));
+        System.out.println(Update_ith_bit(n, i, 1));        
+    }
+}
