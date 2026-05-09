5103ff58e5735399db3254730ab00138dd673196
Akash72077 <sai.sai72077@gmail.com>
2026-05-10 00:26:25 +0530

    written boiler plate code

--- DSA/JavaBits/clear_last_ith_bits.java
+++ DSA/JavaBits/clear_last_ith_bits.java
@@ -1,4 +1,12 @@
+import java.util.Scanner;
 public class clear_last_ith_bits {
     public static void main(String[] args) {
+        // to clear ith bits we have to make and & operation with bit mask
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n=sc.nextInt();
+        System.out.println("Enter postion : ");
+        int i=sc.nextInt();
+        sc.close();
     }
 }
