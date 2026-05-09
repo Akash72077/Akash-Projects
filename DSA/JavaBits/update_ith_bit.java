277ca7a2448e3fe7e42e6d37213c19854d93b84f
Akash72077 <sai.sai72077@gmail.com>
2026-05-09 23:59:11 +0530

    .

--- DSA/JavaBits/update_ith_bit.java
+++ DSA/JavaBits/update_ith_bit.java
@@ -16,6 +16,7 @@ public class update_ith_bit {
             return set_ith_bit(n, i);
         }
     }
+    
     public static void main(String[] args) {
           Scanner sc= new Scanner(System.in);
         System.out.println("Enter a number: ");
