0ac3ee30bc34643c64c2ede8f8b42d56d458ee24
Akash72077 <sai.sai72077@gmail.com>
2026-05-10 22:37:54 +0530

    file created for praticing questions

---
+++ DSA/JavaBits/pratice.java
@@ -0,0 +1,7 @@
+public class pratice {
+    public static void main(String[] args) {
+        //What is the value of x^x for any value of x
+        int x=10;
+        System.out.println(x^x);
+    }
+}
