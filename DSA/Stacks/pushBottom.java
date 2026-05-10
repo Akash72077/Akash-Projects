e5c09d3fc2c42ca939c2ce9fbabd5149d391e226
Akash72077 <sai.sai72077@gmail.com>
2026-05-10 14:42:58 +0530

    created function

--- DSA/Stacks/pushBottom.java
+++ DSA/Stacks/pushBottom.java
@@ -1,5 +1,8 @@
+import java.util.*;
 public class pushBottom {
+    public static void pushAtBottom(Stack<Integer>s, int data){
+
+    }
     public static void main(String[] args) {
-        
     }
 }
