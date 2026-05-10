d0eab88d663864958c58a862090891e8fe693a16
Akash72077 <sai.sai72077@gmail.com>
2026-05-10 13:35:44 +0530

    created array list

--- DSA/Stacks/stack.java
+++ DSA/Stacks/stack.java
@@ -1,7 +1,12 @@
+import java.util.*;
 public class stack {
+    static class Stack{
+        static ArrayList<Integer> list = new ArrayList<>();
+       
+    }
     public static void main(String[] args) {
          //operations of stack :
         // push pop and peek these all are time complexity of O(1)
-        
+       
     }
 }
