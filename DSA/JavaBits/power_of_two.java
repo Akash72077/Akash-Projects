39f86d7dbdf69b459bc4121f8fda95a9e40ab197
Akash72077 <sai.sai72077@gmail.com>
2026-05-10 15:57:59 +0530

    completed code

--- DSA/JavaBits/power_of_two.java
+++ DSA/JavaBits/power_of_two.java
@@ -1,5 +1,13 @@
+import java.util.*;
 public class power_of_two {
+    public static boolean power_of_two(int n){
+        return (n&(n-1))==0;
+    }
 public static void main(String[] args) {
-    
+    Scanner sc= new Scanner(System.in);
+    System.out.println("Enter a number to check is it power of two: ");
+    int n=sc.nextInt();
+    System.out.println(power_of_two(n));
+    sc.close();
 }
 }
