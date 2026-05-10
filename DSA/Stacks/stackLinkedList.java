155ecc51757a024afad69a9e90e7b29bd7f9709b
Akash72077 <sai.sai72077@gmail.com>
2026-05-10 13:56:26 +0530

    written boiler plate code

--- DSA/Stacks/stackLinkedList.java
+++ DSA/Stacks/stackLinkedList.java
@@ -1,5 +1,19 @@
 public class stackLinkedList {
+    static class Node{
+        int data;; 
+        Node next;
+        Node(int data){
+            this.data=data;
+            this.next=null;
+        }
+    }
+    static class Stack{
+        static Node head= null;
+      
+        }
+    
     public static void main(String[] args) {
+        Stack s = new Stack();
         
     }
 }
