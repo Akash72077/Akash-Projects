23bb62c6607fdb159b3e28d91c9188e92c95d493
Akash72077 <sai.sai72077@gmail.com>
2026-05-10 14:35:30 +0530

    completed code

--- DSA/Stacks/StackUsingCollectionFrameWork.java
+++ DSA/Stacks/StackUsingCollectionFrameWork.java
@@ -1,5 +1,18 @@
+import java.util.*;
 public class StackUsingCollectionFrameWork {
     public static void main(String[] args) {
-        
+        Stack<Integer> s= new Stack<>();
+    s.push(1);
+       s.push(2);
+       s.push(4);
+       s.push(5);
+       s.push(7);
+       s.push(9);
+       s.push(11);
+       s.push(13);
+       while(!s.isEmpty()){
+        System.out.println(s.peek());
+        s.pop();
+       }
     }
 }
