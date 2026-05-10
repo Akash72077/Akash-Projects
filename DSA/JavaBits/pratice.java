d09b048b91d75c64b5af689751e297bd8455f95a
Akash72077 <sai.sai72077@gmail.com>
2026-05-10 23:25:52 +0530

    completed code

--- DSA/JavaBits/pratice.java
+++ DSA/JavaBits/pratice.java
@@ -3,5 +3,22 @@ public class pratice {
         //What is the value of x^x for any value of x
         int x=10;
         System.out.println(x^x);
+        // swap two numbersd without using any third varaible 
+        // method-1: 
+        int a=10;
+        int b=20;
+        a=a+b;
+        b=a-b;
+        a=a-b;
+          System.out.println(a+" "+b);
+        // method-2
+        int c=12;
+        int d=25;
+        c=c^d;
+        d=c^d;
+        c=c^d;
+        System.out.println(c+" "+d);
+
+      
     }
 }
