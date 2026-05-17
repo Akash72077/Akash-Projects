d14f2cc7494ec19cf7df671a9d97edf733fbeaac
Akash72077 <sai.sai72077@gmail.com>
2026-05-17 21:01:10 +0530

    completed footer

--- Zerodha/frontend/src/landing_page/Footer.js
+++ Zerodha/frontend/src/landing_page/Footer.js
@@ -1,83 +1,278 @@
-import React from 'react';
+import React from "react";
 function Footer() {
-    return ( 
-        <footer
-         style={{backgroundColor:"#fbfbfb"}}>
-      <div className='container border-top mt-5'
-      style={{backgroundColor:"#fbfbfb"}}>
-        <div className='row mt-5'>
-            <div className='col'>
-            <img src="media/images/logo.svg" style={{width:"50%"}} alt="Logo"/>
-            <p> &copy; 2010-2014, Not Zerodha Broking Ltd.<br/> All rights reserved.</p>
-            </div>
-             <div className='col'>
-                <p  className='fw-bold fs-5' >Account</p>
-                <a href='' className='footer-link'>Open an account</a>
-                <a href='' className='footer-link'>Fund transfer</a>
-                <a href='' className='footer-link'>60 day challenge</a>
-            </div>
-            <div className='col'>
-                <p  className='fw-bold fs-5'>Support</p>
-                <a href='' className='footer-link'>Contact</a>
-                <a href='' className='footer-link'>Support portal</a>
-                <a href='' className='footer-link'>Z-connect blog</a>
-                <a href='' className='footer-link'>List of charges</a>
-                <a href='' className='footer-link'>Download & resources</a>
-                
-            </div>
-            <div className='col'>
-                <p  className='fw-bold fs-5'>Company</p>
-                <a href='' className='footer-link'>About</a>
-                <a href='' className='footer-link'>Products</a>
-                <a href='' className='footer-link'>Pricing</a>
-                <a href='' className='footer-link'>Referral Programs</a>
-                <a href='' className='footer-link'>Careers</a>
-                <a href='' className='footer-link'>Zerodha.tech</a>
-                <a href='' className='footer-link'>Press & media</a>
-                <a href='' className='footer-link'>Zerodha cares(CSR)</a>             
-            </div>
-             <div className='col'>
-                <p  className='fw-bold fs-5'>Quick links</p>
-                <a href='' className='footer-link'>Upcoming IPOs</a>
-                <a href='' className='footer-link'>Brokerage charges</a>
-                <a href='' className='footer-link'>Market holidays</a>
-                 <a href='' className='footer-link'>Economic calendar</a>
-                  <a href='' className='footer-link'>Calculators</a>
-                  <a href='' className='footer-link'>Markets</a>
-                  <a href='' className='footer-link'>Sectors</a>
-                  <a href='' className='footer-link'>Gift Nifty</a>
-            </div>
+  return (
+    <footer style={{ backgroundColor: "#fbfbfb" }}>
+      <div
+        className="container border-top mt-5"
+        style={{ backgroundColor: "#fbfbfb" }}
+      >
+        <div className="row mt-5">
+          <div className="col">
+            <img
+              src="media/images/logo.svg"
+              style={{ width: "50%" }}
+              alt="Logo"
+            />
+            <p>
+              {" "}
+              &copy; 2010-2014, Not Zerodha Broking Ltd.
+              <br /> All rights reserved.
+            </p>
+          </div>
+          <div className="col">
+            <p className="fw-bold fs-5">Account</p>
+            <a href="" className="footer-link">
+              Open an account
+            </a>
+            <a href="" className="footer-link">
+              Fund transfer
+            </a>
+            <a href="" className="footer-link">
+              60 day challenge
+            </a>
+          </div>
+          <div className="col">
+            <p className="fw-bold fs-5">Support</p>
+            <a href="" className="footer-link">
+              Contact
+            </a>
+            <a href="" className="footer-link">
+              Support portal
+            </a>
+            <a href="" className="footer-link">
+              Z-connect blog
+            </a>
+            <a href="" className="footer-link">
+              List of charges
+            </a>
+            <a href="" className="footer-link">
+              Download & resources
+            </a>
+          </div>
+          <div className="col">
+            <p className="fw-bold fs-5">Company</p>
+            <a href="" className="footer-link">
+              About
+            </a>
+            <a href="" className="footer-link">
+              Products
+            </a>
+            <a href="" className="footer-link">
+              Pricing
+            </a>
+            <a href="" className="footer-link">
+              Referral Programs
+            </a>
+            <a href="" className="footer-link">
+              Careers
+            </a>
+            <a href="" className="footer-link">
+              Zerodha.tech
+            </a>
+            <a href="" className="footer-link">
+              Press & media
+            </a>
+            <a href="" className="footer-link">
+              Zerodha cares(CSR)
+            </a>
+          </div>
+          <div className="col">
+            <p className="fw-bold fs-5">Quick links</p>
+            <a href="" className="footer-link">
+              Upcoming IPOs
+            </a>
+            <a href="" className="footer-link">
+              Brokerage charges
+            </a>
+            <a href="" className="footer-link">
+              Market holidays
+            </a>
+            <a href="" className="footer-link">
+              Economic calendar
+            </a>
+            <a href="" className="footer-link">
+              Calculators
+            </a>
+            <a href="" className="footer-link">
+              Markets
+            </a>
+            <a href="" className="footer-link">
+              Sectors
+            </a>
+            <a href="" className="footer-link">
+              Gift Nifty
+            </a>
+          </div>
         </div>
-        <div className='mt-5 text-small text-muted ' style={{ fontSize: '11px' }}>
-        <p >
-            Zerodha Broking Ltd.: Member of NSE, BSE, MCX & MSEI – SEBI Registration no.: INZ000031633 CDSL/NSDL: Depository services through Zerodha Broking Ltd. – SEBI Registration no.: IN-DP-431-2019 Registered Address: Zerodha Broking Ltd., #153/154, 4th Cross, Dollars Colony, Opp. Clarence Public School, J.P Nagar 4th Phase, Bengaluru - 560078, Karnataka, India. For any complaints pertaining to securities broking please write to <a href=''> complaints@zerodha.com</a>, for DP related to<a href=''> dp@zerodha.com</a>. Please ensure you carefully read the Risk Disclosure Document as prescribed by SEBI | ICF
-        </p>
-         <p >
-        Procedure to file a complaint on  <a href=''>SEBI SCORES</a>: Register on SCORES portal. Mandatory details for filing complaints on SCORES: Name, PAN, Address, Mobile Number, E-mail ID. Benefits: Effective Communication, Speedy redressal of the grievances
-        </p>
-         <p > <a href=''>Smart Online Dispute Resolution</a> | <a href=''>Grievances Redressal Mechanism</a></p>
-          <p >
-            Investments in securities market are subject to market risks; read all the related documents carefully before investing.
-        </p>
-        <p >
-            Attention investors: 1) Stock brokers can accept securities as margins from clients only by way of pledge in the depository system w.e.f September 01, 2020. 2) Update your e-mail and phone number with your stock broker / depository participant and receive OTP directly from depository on your e-mail and/or mobile number to create pledge. 3) Check your securities / MF / bonds in the consolidated account statement issued by NSDL/CDSL every month.
-        </p>
-         <p >
-            India's largest broker based on networth as per NSE.  <a href=''>NSE broker factsheet</a>
-        </p>
-          <p >
-           "Prevent unauthorised transactions in your account. Update your mobile numbers/email IDs with your stock brokers/depository participants. Receive information of your transactions directly from Exchange/Depositories on your mobile/email at the end of the day. Issued in the interest of investors. KYC is one time exercise while dealing in securities markets - once KYC is done through a SEBI registered intermediary (broker, DP, Mutual Fund etc.), you need not undergo the same process again when you approach another intermediary." Dear Investor, if you are subscribing to an IPO, there is no need to issue a cheque. Please write the Bank account number and sign the IPO application form to authorize your bank to make payment in case of allotment. In case of non allotment the funds will remain in your bank account. As a business we don't give stock tips, and have not authorized anyone to trade on behalf of others. If you find anyone claiming to be part of Zerodha and offering such services,  <a href=''>please create a ticket here.</a>  
-        </p>
-          <p >
-            *Customers availing insurance advisory services offered by Ditto (Tacterial Consulting Private Limited | IRDAI Registered Corporate Agent (Composite) License No CA0738) will not have access to the exchange investor grievance redressal forum, SEBI SCORES/ODR, or arbitration mechanism for such products.
-        </p>
-        <p >
-            Fixed deposit products offered on this platform are third-party products (TPP) and are not Exchange traded products. These are offered through Blostem Fintech Private Limited. Zerodha Broking Limited (SEBI Registration No.: INZ000031633) is acting solely as a distributor for these products. Any disputes arising with respect to such distribution activity will not have access to SEBI SCORES/ODR, Exchange Investor Grievance Redressal Forum, or Arbitration mechanism. Fixed deposits are regulated by the Reserve Bank of India (RBI).
-        </p>
+        <div
+          className="mt-5 text-small text-muted "
+          style={{ fontSize: "11px" }}
+        >
+          <p
+            style={{
+              color: "#9B9B9B",
+            }}
+          >
+            Zerodha Broking Ltd.: Member of NSE, BSE, MCX & MSEI – SEBI
+            Registration no.: INZ000031633 CDSL/NSDL: Depository services
+            through Zerodha Broking Ltd. – SEBI Registration no.: IN-DP-431-2019
+            Registered Address: Zerodha Broking Ltd., #153/154, 4th Cross,
+            Dollars Colony, Opp. Clarence Public School, J.P Nagar 4th Phase,
+            Bengaluru - 560078, Karnataka, India. For any complaints pertaining
+            to securities broking please write to{" "}
+            <a href="" className="footer-para">
+              {" "}
+              complaints@zerodha.com
+            </a>
+            , for DP related to
+            <a href="" className="footer-para">
+              {" "}
+              dp@zerodha.com
+            </a>
+            . Please ensure you carefully read the Risk Disclosure Document as
+            prescribed by SEBI | ICF
+          </p>
+          <p
+            style={{
+              color: "#9B9B9B",
+            }}
+          >
+            Procedure to file a complaint on{" "}
+            <a href="" className="footer-para">
+              SEBI SCORES
+            </a>
+            : Register on SCORES portal. Mandatory details for filing complaints
+            on SCORES: Name, PAN, Address, Mobile Number, E-mail ID. Benefits:
+            Effective Communication, Speedy redressal of the grievances
+          </p>
+          <p
+            style={{
+              color: "#9B9B9B",
+            }}
+          >
+            {" "}
+            <a href="" className="footer-para">
+              Smart Online Dispute Resolution
+            </a>{" "}
+            |{" "}
+            <a href="" className="footer-para">
+              Grievances Redressal Mechanism
+            </a>
+          </p>
+          <p
+            style={{
+              color: "#9B9B9B",
+            }}
+          >
+            Investments in securities market are subject to market risks; read
+            all the related documents carefully before investing.
+          </p>
+          <p
+            style={{
+              color: "#9B9B9B",
+            }}
+          >
+            Attention investors: 1) Stock brokers can accept securities as
+            margins from clients only by way of pledge in the depository system
+            w.e.f September 01, 2020. 2) Update your e-mail and phone number
+            with your stock broker / depository participant and receive OTP
+            directly from depository on your e-mail and/or mobile number to
+            create pledge. 3) Check your securities / MF / bonds in the
+            consolidated account statement issued by NSDL/CDSL every month.
+          </p>
+          <p
+            style={{
+              color: "#9B9B9B",
+            }}
+          >
+            India's largest broker based on networth as per NSE.{" "}
+            <a href="" className="footer-para">
+              NSE broker factsheet
+            </a>
+          </p>
+          <p
+            style={{
+              color: "#9B9B9B",
+            }}
+          >
+            "Prevent unauthorised transactions in your account. Update your
+            mobile numbers/email IDs with your stock brokers/depository
+            participants. Receive information of your transactions directly from
+            Exchange/Depositories on your mobile/email at the end of the day.
+            Issued in the interest of investors. KYC is one time exercise while
+            dealing in securities markets - once KYC is done through a SEBI
+            registered intermediary (broker, DP, Mutual Fund etc.), you need not
+            undergo the same process again when you approach another
+            intermediary." Dear Investor, if you are subscribing to an IPO,
+            there is no need to issue a cheque. Please write the Bank account
+            number and sign the IPO application form to authorize your bank to
+            make payment in case of allotment. In case of non allotment the
+            funds will remain in your bank account. As a business we don't give
+            stock tips, and have not authorized anyone to trade on behalf of
+            others. If you find anyone claiming to be part of Zerodha and
+            offering such services,{" "}
+            <a href="" className="footer-para">
+              please create a ticket here.
+            </a>
+          </p>
+          <p
+            style={{
+              color: "#9B9B9B",
+            }}
+          >
+            *Customers availing insurance advisory services offered by Ditto
+            (Tacterial Consulting Private Limited | IRDAI Registered Corporate
+            Agent (Composite) License No CA0738) will not have access to the
+            exchange investor grievance redressal forum, SEBI SCORES/ODR, or
+            arbitration mechanism for such products.
+          </p>
+          <p
+            style={{
+              color: "#9B9B9B",
+            }}
+          >
+            Fixed deposit products offered on this platform are third-party
+            products (TPP) and are not Exchange traded products. These are
+            offered through Blostem Fintech Private Limited. Zerodha Broking
+            Limited (SEBI Registration No.: INZ000031633) is acting solely as a
+            distributor for these products. Any disputes arising with respect to
+            such distribution activity will not have access to SEBI SCORES/ODR,
+            Exchange Investor Grievance Redressal Forum, or Arbitration
+            mechanism. Fixed deposits are regulated by the Reserve Bank of India
+            (RBI).
+          </p>
+        </div>
+        <div className="text-center mt-4 py-3">
+          <a href="" className="footer-last">
+            NSE
+          </a>
+          <a href="" className="footer-last">
+            BSE
+          </a>
+          <a href="" className="footer-last">
+            MCX
+          </a>
+          <a href="" className="footer-last">
+            Terms & conditions{" "}
+          </a>
+          <a href="" className="footer-last">
+            Policies & procedures{" "}
+          </a>
+          <a href="" className="footer-last">
+            Privacy and Policiy
+          </a>
+          <a href="" className="footer-last">
+            Disclosure{" "}
+          </a>
+          <a href="" className="footer-last">
+            For investor's attention
+          </a>
+          <a href="" className="footer-last">
+            Investor charter
+          </a>
         </div>
       </div>
-      </footer>
-     );
+    </footer>
+  );
 }
-
 export default Footer;
