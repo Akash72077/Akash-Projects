23831d08e29794e35c083034b13efaa5587a51f0
Akash72077 <sai.sai72077@gmail.com>
2026-05-17 21:20:51 +0530

    imported all

--- Zerodha/frontend/src/index.js
+++ Zerodha/frontend/src/index.js
@@ -1,11 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./index.css";
 import HomePage from "./landing_page/home/HomePage";
-
+import Signup  from "./landing_page/signup/Signup";
+import PricingPage from "./landing_page/pricing/PricingPage";
+import SupportPage from "./landing_page/support/SupportPage"
+import ProductsPage from "./landing_page/products/ProductsPage"
+import AboutPage from "./landing_page/about/AboutPage"
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <HomePage />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<HomePage />}></Route>
+      <Route path="/signup" element={<Signup />}></Route>
+       <Route path="/about" element={<AboutPage />}></Route>
+      <Route path="/product" element={<ProductsPage />}></Route>
+      <Route path="/pricing" element={<PricingPage />}></Route>
+      <Route path="/support" element={<SupportPage />}></Route>
+    </Routes>
+  </BrowserRouter>,
 );
