04bc35b3512c700a588b0ab9a02fa8d58103f09d
Akash72077 <sai.sai72077@gmail.com>
2026-05-18 13:03:28 +0530

    completed hero in products

--- Zerodha/frontend/src/landing_page/products/Hero.js
+++ Zerodha/frontend/src/landing_page/products/Hero.js
@@ -1,8 +1,47 @@
-import React from 'react';
+import React from "react";
 function Hero() {
-    return ( 
-        <h1>Hero</h1>
-     );
-}
+  return (
+    <div className="border-bottom">
+      <div className="container text-center " style={{ padding: "100px 20px" }}>
+        <h1
+          className="mt-2"
+          style={{
+            fontSize: "28px",
+            color: "#424242",
+            fontFamily: "sans-serif",
+            fontWeight: "500",
+          }}
+        >
+          Zerodha Products
+        </h1>
+        <p
+          className="mt-2"
+          style={{
+            fontSize: "20px",
+            color: "#424242",
+            fontFamily: "sans-serif",
+          }}
+        >
+          Sleek, modern, and intuitive trading platforms
+        </p>
 
+        <p
+          className="mt-2"
+          style={{
+            fontSize: "16px",
+            color: "#424242",
+            fontFamily: "sans-serif",
+            fontWeight: "500",
+          }}
+        >
+          Check out our{" "}
+          <a href="" style={{ textDecoration: "none", fontWeight: "500" }}>
+            investment offerings{" "}
+            <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
+          </a>
+        </p>
+      </div>
+    </div>
+  );
+}
 export default Hero;
