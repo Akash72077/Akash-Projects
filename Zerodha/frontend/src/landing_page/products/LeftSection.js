b894b4b7067bde987815e337e64493ce1a4bb030
Akash72077 <sai.sai72077@gmail.com>
2026-05-18 17:53:36 +0530

    completed left side

--- Zerodha/frontend/src/landing_page/products/LeftSection.js
+++ Zerodha/frontend/src/landing_page/products/LeftSection.js
@@ -1,4 +1,5 @@
 import React from "react";
+
 function LeftSection({
   imageURL,
   productName,
@@ -7,14 +8,19 @@ function LeftSection({
   learnMore,
   googlePlay,
   appStore,
+  showDemoLinks,
+  coinLink,
 }) {
   return (
     <div className="container">
       <div className="row p-5">
         <div className="col-6 p-5">
-          <img src={imageURL} />
+          <img src={imageURL} alt="Product" style={{ maxWidth: "100%" }} />
         </div>
-        <div className="col-6 p-5 mt-5" style={{padding:"10px"}}>
+
+        <div className="col-1"></div>
+
+        <div className="col-5 mt-5" style={{ padding: "50px" }}>
           <h2
             className="mt-2"
             style={{
@@ -26,6 +32,7 @@ function LeftSection({
           >
             {productName}
           </h2>
+
           <p
             className="mt-2"
             style={{
@@ -36,25 +43,60 @@ function LeftSection({
           >
             {productDescription}
           </p>
-          <div>
-            <a href={tryDemo} style={{ textDecoration: "none" }}>
-              Try demo {" "}
-              <i class="fa fa-long-arrow-right" aria-hidden="true"></i>
-            </a>
-            <a
-              href={learnMore}
-              style={{ marginLeft: "5.5rem", textDecoration: "none" }}
-            >
-              Learn more
-               {" "}<i class="fa fa-long-arrow-right" aria-hidden="true"></i>
-            </a>
-          </div>
+
+          {/* Try Demo & Learn More */}
+          {showDemoLinks && (
+            <div>
+              <a href={tryDemo} style={{ textDecoration: "none" }}>
+                Try demo{" "}
+                <i className="fa fa-long-arrow-right" aria-hidden="true"></i>
+              </a>
+
+              <a
+                href={learnMore}
+                style={{
+                  marginLeft: "5.5rem",
+                  textDecoration: "none",
+                }}
+              >
+                Learn more{" "}
+                <i className="fa fa-long-arrow-right" aria-hidden="true"></i>
+              </a>
+            </div>
+          )}
+
+          {/* Coin Link */}
+          {coinLink && (
+            <div className="mt-3">
+              <a href="" style={{ textDecoration: "none" }}>
+                Coin{" "}
+                <i className="fa fa-long-arrow-right" aria-hidden="true"></i>
+              </a>
+            </div>
+          )}
+
+          {/* App Store Buttons */}
           <div className="mt-4">
             <a href={googlePlay}>
-              <img src="media/images/googlePlayBadge.svg" style={{maxWidth:"100%", height:"45px"}} />
+              <img
+                src="media/images/googlePlayBadge.svg"
+                alt="Google Play"
+                style={{
+                  maxWidth: "100%",
+                  height: "45px",
+                }}
+              />
             </a>
+
             <a href={appStore} style={{ marginLeft: "25px" }}>
-              <img src="media/images/appstoreBadge.svg" style={{maxWidth:"100%", height:"45px"}}  />
+              <img
+                src="media/images/appstoreBadge.svg"
+                alt="App Store"
+                style={{
+                  maxWidth: "100%",
+                  height: "45px",
+                }}
+              />
             </a>
           </div>
         </div>
@@ -62,4 +104,5 @@ function LeftSection({
     </div>
   );
 }
+
 export default LeftSection;
