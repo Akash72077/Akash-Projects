d4518a009f6ae0021e5335a48833c9104e94fae8
Akash72077 <sai.sai72077@gmail.com>
2026-05-19 20:48:44 +0530

    completed brokerage page

--- Zerodha/frontend/src/landing_page/pricing/Brokerage.js
+++ Zerodha/frontend/src/landing_page/pricing/Brokerage.js
@@ -5,12 +5,12 @@ function Brokerage() {
     <div className="container">
       <div className="row p-5 mt-5  text-center">
         <div className="col-8 p-4 ">
-          <h3 className="fs-4"  style={{color:"#387ED1"}}>
+          <h3 className="fs-4 mb-4"  style={{color:"#387ED1"}}>
             <a href="" style={{ textDecoration: "none" }}>
               Brokerage calculator
             </a>
           </h3>
-          <ul className="text-muted" style={{textAlign:"left"}}>
+          <ul className="text-muted" style={{textAlign:"left", lineHeight:"2.5", fontSize:"14px"}}>
             <li>
               Call & Trade and RMS auto-squareoff: Additional charges of ₹50 +
               GST per order.
@@ -18,7 +18,7 @@ function Brokerage() {
             <li>Digital contract notes will be sent via e-mail.</li>
             <li>
               Physical copies of contract notes, if required, shall be charged
-              ₹20 per contraa=ct note. Courier charges apply.
+              ₹20 per contract note. Courier charges apply.
             </li>
             <li>
               For NRI account (non-PIS), 0.5% or &#8377;100 per executed order
