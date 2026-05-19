1ac7b8dad5be4e954b0ab71919d6616992da8ee3
Akash72077 <sai.sai72077@gmail.com>
2026-05-19 14:29:43 +0530

    done

--- Zerodha/frontend/src/landing_page/home/Hero.js
+++ Zerodha/frontend/src/landing_page/home/Hero.js
@@ -1,12 +1,15 @@
 import React from "react";
-
 function Hero() {
   return (
     //p-5 is a class in bootstrap which provides padding of 5%
     <div className="container p-5">
       <div className="row text-center">
-       
-
+        {/* //mb-5 is a class in bootstrap which provides margin buttom of 5% */}
+        <img
+          src="media/images/homeHero.png"
+          alt="Hero image"
+          className="mb-5"
+        />
         <h1 className="mt-5">Invest in everything</h1>
         <p className="p-2">
           Online platform to invest in stocks, derivatives, mutual funds, and
@@ -23,5 +26,4 @@ function Hero() {
     </div>
   );
 }
-
 export default Hero;
