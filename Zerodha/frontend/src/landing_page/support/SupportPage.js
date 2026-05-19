642ed677dde86ed2ca584ee0567322148e7ef4e6
Akash72077 <sai.sai72077@gmail.com>
2026-05-19 21:36:00 +0530

    added components

--- Zerodha/frontend/src/landing_page/support/SupportPage.js
+++ Zerodha/frontend/src/landing_page/support/SupportPage.js
@@ -2,7 +2,11 @@ import React from "react";
 import Hero from "./Hero";
 import CreateTicket from "./CreateTicket";
 const SupportPage = () => {
-  return <div>SupportPage</div>;
+  return (<>
+    <hero/>
+    <CreateTicket/>
+    </>
+  )
 };
 
 export default SupportPage;
