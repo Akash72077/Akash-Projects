4bffae44a2f80ad8393922fa0582643b83ec3911
Akash72077 <sai.sai72077@gmail.com>
2026-05-21 20:57:12 +0530

    addded .env and app.listen

--- Zerodha/backend/index.js
+++ Zerodha/backend/index.js
@@ -1,2 +1,9 @@
-const express=require("express");
-const app=express();
+require("dotenv").config();
+
+const express = require("express");
+
+const app = express();
+
+app.listen(3002, () => {
+  console.log("App started!");
+});
