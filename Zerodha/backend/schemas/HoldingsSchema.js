6afff3cb63487be5ca71cb177a3ea9debb06fd55
Akash72077 <sai.sai72077@gmail.com>
2026-05-24 16:44:01 +0530

    added datatypes for schema

--- Zerodha/backend/schemas/HoldingsSchema.js
+++ Zerodha/backend/schemas/HoldingsSchema.js
@@ -1,3 +1,10 @@
 const { Schema } = require("mongoose");
-const HoldingsSchema = new Schema({});
+const HoldingsSchema = new Schema({
+  name: String,
+  qty: Number,
+  avg: Number,
+  price: Number,
+  net: String,
+  day: String,
+});
 module.exports = {};
