0595eef2034d9197f389d3c283b728d264fad084
Akash72077 <sai.sai72077@gmail.com>
2026-05-25 15:37:56 +0530

    added spacing in code

--- Zerodha/backend/index.js
+++ Zerodha/backend/index.js
@@ -1,9 +1,11 @@
 require("dotenv").config();
+
 const express = require("express");
 const mongoose = require("mongoose");
 
 const PORT = process.env.PORT || 3002;
 const url = process.env.MONGO_URL;
+
 const app = express();
 
 app.listen(PORT, () => {
