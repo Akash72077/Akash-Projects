5c10a61a50d93dc62c8c0502b4dfab567dac6a8a
Akash72077 <sai.sai72077@gmail.com>
2026-05-25 20:38:57 +0530

    added post route for new order

--- Zerodha/backend/index.js
+++ Zerodha/backend/index.js
@@ -8,7 +8,7 @@ const cors = require("cors");
 
 const { HoldingsModel } = require("./model/HoldingsModel");
 const { PositionsModel } = require("./model/PositionsModel");
-
+const { OrdersModel } = require("./model/OrdersModel");
 const PORT = process.env.PORT || 3002;
 const url = process.env.MONGO_URL;
 
@@ -193,6 +193,18 @@ app.get("/allPositions", async (req, res) => {
   res.json(allPositions);
 });
 
+app.post("/newOrder", async (req, res) => {
+  let newOrder = new OrdersModel({
+    name: req.body.name,
+    qty: req.body.qty,
+    price: req.body.price,
+    mode: req.body.mode,
+  });
+
+  newOrder.save();
+
+  res.send("Order saved");
+});
 app.listen(PORT, () => {
   console.log("App started!");
   mongoose.connect(url);
