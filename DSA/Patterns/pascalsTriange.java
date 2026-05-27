c19ad577dc539c530fc968993d5966453d6f0782
Akash72077 <sai.sai72077@gmail.com>
2026-05-27 23:42:52 +0530

    added

--- DSA/Patterns/pascalsTriange.java
+++ DSA/Patterns/pascalsTriange.java
@@ -1,4 +1,13 @@
+import java.util.Scanner;
+import java.util.List;
+
+ public static List<List<Interger>> Generate(int rows){
+
+ }
+
 public class pascalsTriange {
 public static void main(String[] args) {
+    Scanner sc= new Scanner(System.in);
+    int n=sc.nextInt();
 }
 }
