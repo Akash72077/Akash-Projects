5fcdb985d408ab1f61c1b23d53a0d18a4eaeb56d
Akash72077 <sai.sai72077@gmail.com>
2026-05-28 16:48:48 +0530

    fix: add missing import statement for Map and clean up whitespace in LearnMap4.java

--- DSA/Hashing/LearnMap4.java
+++ DSA/Hashing/LearnMap4.java
@@ -1,4 +1,5 @@
 import java.util.HashMap;
+import java.util.Map;
 
 public class LearnMap4 {
     public static void main(String[] args) {
@@ -8,9 +9,16 @@ public class LearnMap4 {
         fruitCount.put("Orange",15); 
         fruitCount.put("Banana",20);
         fruitCount.put("Mango",11);
+
+
         // iterating values using for each
         for(String i:fruitCount.keySet()){
             System.out.println("Key: "+i+" & Value: "+fruitCount.get(i));
+        }
+
+        //iterating using map and entry set
+        for(Map.Entry<String,Integer> i:fruitCount.entrySet()){
+            System.out.println("Key: "+i.getKey()+", Value: "+i.getValue());
         }
     }
 }
