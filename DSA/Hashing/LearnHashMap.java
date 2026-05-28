c7614d17948c703bfc801345a15242913d5c01fd
Akash72077 <sai.sai72077@gmail.com>
2026-05-28 12:30:30 +0530

    changed file name

--- DSA/Hashing/LearnHashMap.java
+++ DSA/Hashing/LearnHashMap.java
@@ -1,5 +1,5 @@
 import java.util.HashMap;
-public class Demo {
+public class LearnHashMap {
     public static void main(String[] args) {
         HashMap<Integer, Integer> hm= new HashMap<>();
         // here hm is the name of hashmap 
