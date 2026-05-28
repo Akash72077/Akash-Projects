5801fbaff0549e76f914fc9ee01a6fc3907c7c60
Akash72077 <sai.sai72077@gmail.com>
2026-05-28 16:44:18 +0530

    feat: implement main method in LearnMap4 class with fruit count example

--- DSA/Hashing/LearnMap4.java
+++ DSA/Hashing/LearnMap4.java
@@ -1,4 +1,12 @@
+import java.util.HashMap;
+
 public class LearnMap4 {
     public static void main(String[] args) {
+        HashMap<String,Integer> fruitCount= new HashMap<>();
+
+        fruitCount.put("Apple", 10);
+        fruitCount.put("Orange",15); 
+        fruitCount.put("Banana",20);
+        fruitCount.put("Mango",11);
     }
 }
