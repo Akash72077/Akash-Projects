ea12fb2ccf53a02a58a2596650aae0bd9010ff9a
Akash72077 <sai.sai72077@gmail.com>
2026-05-28 12:30:02 +0530

    added common keywords in questions

--- DSA/Hashing/Demo.java
+++ DSA/Hashing/Demo.java
@@ -6,7 +6,7 @@ public class Demo {
         // in <Integer(1), Integer(2)> : here 1 is the data type of key and 2 is the data type for value  
 
 
-        
+
         // Hashmap is used when they ask for:
         // fast searching or lookup
         // store key-value pairs 
@@ -14,6 +14,14 @@ public class Demo {
         // //check duplicates 
         // store relationships/mappings
         // find element quickly in O(1) time complexity
+
+        // Common keywords in questions 
+        // frequencies/count
+        // duplicates
+        // lookup/search
+        // pair/sum
+        // mapping
+        // occurrence
 
     }
 }
