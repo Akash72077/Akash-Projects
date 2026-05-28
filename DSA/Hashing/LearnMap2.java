c854a007ae313060e0edbeafa5d9f3f935d8b2ab
Akash72077 <sai.sai72077@gmail.com>
2026-05-28 13:14:28 +0530

    writen a sample code

--- DSA/Hashing/LearnMap2.java
+++ DSA/Hashing/LearnMap2.java
@@ -2,5 +2,10 @@ import java.util.HashMap;
 public class LearnMap2 {
     public static void main(String[] args) {
         HashMap<Integer, Integer> hm =new HashMap<>();
+        hm.put(1,100);
+        hm.put(1,1000);
+        int temp=hm.get(1);
+        hm.put(1,1000/10);
+        System.out.println(temp);
     }
 }
