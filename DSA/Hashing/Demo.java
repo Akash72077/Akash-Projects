7a99a2b9d1f43a2fa2fa239b78706631fc2bc49e
Akash72077 <sai.sai72077@gmail.com>
2026-05-28 12:28:42 +0530

    added how to identify probelms as the commont line

--- DSA/Hashing/Demo.java
+++ DSA/Hashing/Demo.java
@@ -4,5 +4,16 @@ public class Demo {
         HashMap<Integer, Integer> hm= new HashMap<>();
         // here hm is the name of hashmap 
         // in <Integer(1), Integer(2)> : here 1 is the data type of key and 2 is the data type for value  
+
+
+        
+        // Hashmap is used when they ask for:
+        // fast searching or lookup
+        // store key-value pairs 
+        //count frequencies
+        // //check duplicates 
+        // store relationships/mappings
+        // find element quickly in O(1) time complexity
+
     }
 }
