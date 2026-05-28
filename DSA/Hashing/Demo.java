9f56bb01129695daccd222e99907b051141c1da5
Akash72077 <sai.sai72077@gmail.com>
2026-05-28 12:26:18 +0530

    added note as comment line

--- DSA/Hashing/Demo.java
+++ DSA/Hashing/Demo.java
@@ -2,5 +2,7 @@ import java.util.HashMap;
 public class Demo {
     public static void main(String[] args) {
         HashMap<Integer, Integer> hm= new HashMap<>();
+        // here hm is the name of hashmap 
+        // in <Integer(1), Integer(2)> : here 1 is the data type of key and 2 is the data type for value  
     }
 }
