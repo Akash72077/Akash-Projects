471a515c04f7bc771e7ad625c7c136a548231a54
Akash72077 <sai.sai72077@gmail.com>
2026-05-28 13:52:01 +0530

    implement main method in LearnMap3 class with HashMap example

--- DSA/Hashing/LearnMap3.java
+++ DSA/Hashing/LearnMap3.java
@@ -1,4 +1,18 @@
+import java.util.HashMap;
+import java.util.Set;
 public class LearnMap3 {
     public static void main(String[] args) {
+        HashMap<Integer,Integer> hm= new HashMap<>();
+        Set<Integer>keys=hm.keySet();
+        hm.put(1,20);
+        hm.put(2,40);
+        hm.put(6,50);
+        hm.put(4,60);
+        hm.put(9,90);
+        //key set makes all the keys in a hashmap as a set
+        for(int i:hm.keySet()){
+            System.out.println(i+"->"+hm.get(i));
+        }
+        System.out.println(keys);
     }
 }
