2a77fcb4bb2d5e907daf6c4b2993c23fddcc8419
Akash <sai.sai72077@gmail.com>
2026-05-29 23:18:16 +0530

    Enhance comments in LearnHashMap.java
    Added comments explaining the purpose and usage of HashMap.

--- DSA/Hashing/LearnHashMap.java
+++ DSA/Hashing/LearnHashMap.java
@@ -4,7 +4,6 @@ public class LearnHashMap {
         HashMap<Integer, Integer> hm= new HashMap<>();
         // here hm is the name of hashmap 
         // in <Integer(1), Integer(2)> : here 1 is the data type of key and 2 is the data type for value  
-
         // Hashmap is used when they ask for:
         // fast searching or lookup
         // store key-value pairs 
