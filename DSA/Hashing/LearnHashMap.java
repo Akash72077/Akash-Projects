8107dcc48ef083b79f194021a13ff35474fedbbf
Akash <sai.sai72077@gmail.com>
2026-05-29 23:17:59 +0530

    Clean up comments in LearnHashMap.java
    Removed unnecessary comments and whitespace.

--- DSA/Hashing/LearnHashMap.java
+++ DSA/Hashing/LearnHashMap.java
@@ -5,8 +5,6 @@ public class LearnHashMap {
         // here hm is the name of hashmap 
         // in <Integer(1), Integer(2)> : here 1 is the data type of key and 2 is the data type for value  
 
-
-
         // Hashmap is used when they ask for:
         // fast searching or lookup
         // store key-value pairs 
