2482b6b1ac0202904af5a2912177c34f1b61ed90
Akash72077 <sai.sai72077@gmail.com>
2026-05-30 18:14:56 +0530

    created objected and called both sub and add fiunctions

--- DSA/Oops/Inheritance.java
+++ DSA/Oops/Inheritance.java
@@ -13,6 +13,9 @@ class Calculator{
 
 public class Inheritance {
     public static void main(String[] args) {
-        //Dfination : inheritance is the aquiring properties from one class to another class 
+        //Dfination : inheritance is the aquiring properties from one class to another class
+        Calculator cal= new Calculator(); 
+        System.out.println(cal.add(1, 6));
+        System.out.println(cal.sub(100, 6));
     }
 }
