b359f2b1803c41584c0ef36ae1a0ecaa8bc485b0
Akash72077 <sai.sai72077@gmail.com>
2026-05-30 19:23:10 +0530

    new code added to accces advanced calcualtor

--- DSA/Oops/Inheritance.java
+++ DSA/Oops/Inheritance.java
@@ -1,8 +1,12 @@
 public class Inheritance {
     public static void main(String[] args) {
         //Dfination : inheritance is the aquiring properties from one class to another class
-        Calculator cal= new Calculator(); 
-        System.out.println(cal.add(1, 6));
-        System.out.println(cal.sub(100, 6));
+        // Calculator cal= new Calculator(); 
+        // System.out.println(cal.add(1, 6));
+        // System.out.println(cal.sub(100, 6));
+        // now we cannot access the operations of calculator 
+         AdvCal cal= new AdvCal(); 
+        System.out.println(cal.mul(1, 6));
+        System.out.println(cal.div(100, 6));
     }
 }
