cd0ee8dd2c8d87db02c77824c683c07287310054
Akash72077 <sai.sai72077@gmail.com>
2026-05-30 17:10:46 +0530

    A code written for Encapsulation

--- DSA/Oops/Encapsulation.java
+++ DSA/Oops/Encapsulation.java
@@ -1,5 +1,29 @@
+class Human{
+    private int age;
+    private String name;
+    public void setAge(int age){
+    this.age=age;  
+    }
+
+    public int getAge(){
+        return age;
+    }
+    
+    public void setName(String name){
+    this.name=name;  
+    }
+    public String getName(){
+        return name;
+    }
+    
+}
+
 public class Encapsulation {
     public static void main(String[] args) {
-        
+        Human Akash = new Human();
+        Akash.setAge(10);
+        System.out.println(Akash.getAge());
+        Akash.setName("Akash");
+        System.out.println(Akash.getName());
     }
 }
