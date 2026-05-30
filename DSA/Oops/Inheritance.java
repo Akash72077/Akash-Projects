ac21f27c90448f78b0058659abda74edf71f1d19
Akash72077 <sai.sai72077@gmail.com>
2026-05-30 18:09:28 +0530

    added defination

--- DSA/Oops/Inheritance.java
+++ DSA/Oops/Inheritance.java
@@ -1,5 +1,5 @@
 public class Inheritance {
     public static void main(String[] args) {
-        //Defination : inheritance is the aquiring properties from one class to another class 
+        //Dfination : inheritance is the aquiring properties from one class to another class 
     }
 }
