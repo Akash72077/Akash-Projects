0bc1da691dfc2c24d54d28b16289f5772f05966f
Akash72077 <sai.sai72077@gmail.com>
2026-05-30 18:12:01 +0530

    added  add and sub method

--- DSA/Oops/Inheritance.java
+++ DSA/Oops/Inheritance.java
@@ -1,3 +1,16 @@
+class Calculator{
+    public int add(int n1, int n2)
+    {
+        return n1+n2;
+    }
+      public int sub(int n1, int n2)
+    {
+        return n1-n2;
+    
+    }
+}
+
+
 public class Inheritance {
     public static void main(String[] args) {
         //Dfination : inheritance is the aquiring properties from one class to another class 
