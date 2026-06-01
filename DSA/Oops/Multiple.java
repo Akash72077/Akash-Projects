65d0a90920351f2a30cc81df58e61226e8b3bdb8
Akash72077 <sai.sai72077@gmail.com>
2026-06-01 21:14:27 +0530

    added classes

--- DSA/Oops/Multiple.java
+++ DSA/Oops/Multiple.java
@@ -1,3 +1,20 @@
+ class A {
+    
+}
+
+ class B  {
+    
+}
+
+//  class C extends A,B {
+     // multiple inheritance will not work in java
+// }
+
+ class C extends A {
+
+}
+
+
 public class Multiple {
 public static void main(String[] args) {
     
