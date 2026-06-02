be5b9ce7eca6decf4c332f7bdfe8805720b36b9f
Akash72077 <sai.sai72077@gmail.com>
2026-06-02 18:19:41 +0530

    added 2 functiosn

--- DSA/Oops/Polymorphism.java
+++ DSA/Oops/Polymorphism.java
@@ -1,4 +1,21 @@
+ class A {
+
+    public void show(){
+          System.out.println("From class A");
+
+    }
+}
+class B extends A {
+
+    public void show(){
+        System.out.println("From class B");
+    }
+}
 public class Polymorphism {
     public static void main(String[] args) {
+
+        B obj1= new B();// as per method overriding concept the output will be from class B
+        obj1.show();
+        
     }
 }
