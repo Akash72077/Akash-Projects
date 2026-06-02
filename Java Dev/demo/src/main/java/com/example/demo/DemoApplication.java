eaab424fb50342022f34367822916168bc16c2e0
Akash72077 <sai.sai72077@gmail.com>
2026-06-02 16:51:25 +0530

    added another line for testing

--- Java Dev/demo/src/main/java/com/example/demo/DemoApplication.java
+++ Java Dev/demo/src/main/java/com/example/demo/DemoApplication.java
@@ -9,6 +9,8 @@ public class DemoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 		System.out.println("Hello world");
+		int a=10;
+		System.out.println(a);
 	}
 
 }
