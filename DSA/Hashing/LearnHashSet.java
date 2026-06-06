43e6bd773bcc926a14cbc03b44a42cc648d475e0
Akash72077 <sai.sai72077@gmail.com>
2026-06-06 15:37:34 +0530

    values inserted using add

--- DSA/Hashing/LearnHashSet.java
+++ DSA/Hashing/LearnHashSet.java
@@ -1,5 +1,10 @@
+import java.util.HashSet;
 public class LearnHashSet {
 public static void main(String[] args) {
-    
+    HashSet<String> st= new HashSet<>();
+    st.add("sai");
+    st.add("akash");
+    st.add("ramesh");
+    st.add("ganesh");
 }
 }
