ee86b5912fafc892823ca61f4a34d86cc4877967
Akash72077 <sai.sai72077@gmail.com>
2026-06-10 17:18:05 +0530

    removed old code

--- DSA/Pratice/LearnJava2.java
+++ DSA/Pratice/LearnJava2.java
@@ -1,23 +1,5 @@
-class addtion {
-    // construtor without parameters or default constructor 
-        addtion(){
-            System.out.println("Constructor is called");
-         }
-          // construtor with parameters or explicit constructor
-         addtion(int a, int b){
-            System.out.println(a+b);
-         }
-          addtion(double a, double b){
-            System.out.println(a+b);
-         }
-}
 public class LearnJava2 {
     public static void main(String[] args) {
-        // className obj = new ClassName();
-        addtion add= new addtion();
-        addtion odd2= new addtion(1,2);
-        addtion odd3= new addtion(1.5,2);
-
-
+        
     }
 }
