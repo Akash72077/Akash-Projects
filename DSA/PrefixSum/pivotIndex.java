09866777c1e3cb2b09903832b78881f523b8d1bb
Akash72077 <sai.sai72077@gmail.com>
2026-06-15 23:32:33 +0530

    code written for pivot index

--- DSA/PrefixSum/pivotIndex.java
+++ DSA/PrefixSum/pivotIndex.java
@@ -1,5 +1,19 @@
-public class pivotIndex {
+public class PivotIndex {
     static int findPivot(int arr[]){
+        int rightSum=0;
+        int leftSum=0;
+        int n=arr.length;
+        for(int var:arr){
+            rightSum+=var;
+        }
+        for(int i=0; i<n; i++){
+            int val=arr[i];
+        rightSum-=val;
+        if(leftSum==rightSum){
+            return i;
+        }
+        leftSum+=val;
+        }
         return -1;
     }
     public static void main(String[] args) {
