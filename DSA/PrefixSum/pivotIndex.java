ad3f333c964a13e03884a8460ecccebf18c0835b
Akash72077 <sai.sai72077@gmail.com>
2026-06-15 23:28:22 +0530

    created new function to find pivot index

--- DSA/PrefixSum/pivotIndex.java
+++ DSA/PrefixSum/pivotIndex.java
@@ -1,5 +1,9 @@
 public class pivotIndex {
+    static int findPivot(int arr[]){
+        return -1;
+    }
     public static void main(String[] args) {
-        
+        int arr[]= {1,7,3,6,5,6};
+        System.out.println("Pivot Index is: "+findPivot(arr));
     }
 }
