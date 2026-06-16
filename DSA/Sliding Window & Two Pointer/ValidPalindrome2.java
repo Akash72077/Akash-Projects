e375c32510519917eee4d091eb6454e3bcec2e6f
Akash72077 <sai.sai72077@gmail.com>
2026-06-16 21:15:33 +0530

    code completed for ValidPalindrome

--- DSA/Sliding Window & Two Pointer/ValidPalindrome2.java
+++ DSA/Sliding Window & Two Pointer/ValidPalindrome2.java
@@ -22,9 +22,46 @@ Output: false
 
 */
 
-
+import java.util.Scanner;
 public class ValidPalindrome2 {
+
+    public static boolean isPalindrome(String s, int left, int right){
+
+            while(left<right){
+            if(s.charAt(left)==s.charAt(right)){
+                left++;
+                right--;
+            }else{
+                return false;
+            }
+        }
+        return true;
+
+    }
+    public static boolean ValidPalindrome(String s){
+
+        int left=0;
+        int right= s.length()-1;
+            while(left<right){
+            if(s.charAt(left)==s.charAt(right)){
+                left++;
+                right--;
+            }else{
+                return isPalindrome(s, left+1, right)|| isPalindrome(s, left, right-1);
+            }
+        }
+        return true;
+    }
     public static void main(String[] args) {
         
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter a string to check is palindrome: ");
+        String s= sc.nextLine();
+        if(ValidPalindrome(s)){
+            System.out.println("Given string is Palindrome.");
+        }else{
+            System.out.println("Given string is not Palindrome.");
+        }
+        sc.close();
     }
 }
