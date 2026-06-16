545efabd1102cc60d3554d4ef155efc64c07c4cb
Akash72077 <sai.sai72077@gmail.com>
2026-06-16 20:13:45 +0530

    problem statement is added to given question

--- DSA/Sliding Window & Two Pointer/IsPalindrome2.java
+++ DSA/Sliding Window & Two Pointer/IsPalindrome2.java
@@ -1,5 +1,26 @@
 
-/*Probelm statement: Given a string s, return true if the s can be palindrome after deleting at most one character from it. */
+/* Leetcode Problem number 680
+
+Problem statement: Given a string s, return true if the s can be palindrome after deleting at most one character from it.
+test cases 
+Example 1:
+
+Input: s = "aba"
+Output: true
+
+Example 2:
+
+Input: s = "abca"
+Output: true
+Explanation: You could delete the character 'c'.
+
+
+Example 3:
+
+Input: s = "abc"
+Output: false
+
+*/
 
 
 public class IsPalindrome2 {
