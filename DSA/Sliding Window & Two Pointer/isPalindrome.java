eef0d4df01a08c552f46df41c1b04d734c4103a3
Akash72077 <sai.sai72077@gmail.com>
2026-06-16 20:08:04 +0530

    A complete code written for is Palindrome

--- DSA/Sliding Window & Two Pointer/isPalindrome.java
+++ DSA/Sliding Window & Two Pointer/isPalindrome.java
@@ -1,5 +1,30 @@
+import java.util.*;
+
 public class isPalindrome {
+
+    public static boolean isPalindrome(String s){
+        int left=0;
+        int right=s.length()-1;
+        while(left<right){
+            if(s.charAt(left)==s.charAt(right)){
+                left++;
+                right--;
+            }else{
+                return false;
+            }
+
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
-        
+        Scanner sc= new Scanner(System.in);
+        String s=sc.nextLine();
+        if(isPalindrome(s)){
+            System.out.println("Given string is Palindrome");
+        }else{
+            System.out.println("Given string is not Palindrome");
+        }
     }
 }
