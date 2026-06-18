407107fc13f6a01ceef3b121b23fea93e4126f49
Akash72077 <sai.sai72077@gmail.com>
2026-06-18 19:23:29 +0530

    code completed

--- DSA/Greedy/JumpGame.java
+++ DSA/Greedy/JumpGame.java
@@ -3,9 +3,16 @@ public class JumpGame {
         int arr[]= {1,1,2,5,2,1,0,0,1,3};
         int n=arr.length;
         int finalPosition=n-1;
-
         for(int idx=n-2; idx>=0; idx--){
+            if(arr[idx]+idx>=finalPosition){
+                finalPosition=idx;
+            }
             
+        }
+        if(finalPosition==0){
+            System.out.println(true);
+        }else{
+            System.out.println(false);
         }
     }
 }
