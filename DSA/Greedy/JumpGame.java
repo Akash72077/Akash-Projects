8dc71b36040c6562114c7123575fc3c16914889e
Akash72077 <sai.sai72077@gmail.com>
2026-06-18 19:21:03 +0530

    a new file created for jump game

---
+++ DSA/Greedy/JumpGame.java
@@ -0,0 +1,11 @@
+public class JumpGame {
+    public static void main(String[] args) {
+        int arr[]= {1,1,2,5,2,1,0,0,1,3};
+        int n=arr.length;
+        int finalPosition=n-1;
+
+        for(int idx=n-2; idx>=0; idx--){
+            
+        }
+    }
+}
