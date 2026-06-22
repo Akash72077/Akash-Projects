9f953a6d24c31d9ccb31f69c5e80d4285c0c1e8b
Akash <sai.sai72077@gmail.com>
2026-06-22 23:54:25 +0530

    Add h2 element query selector in app.js
    Added a query selector for the h2 element to the app.js file.

--- Projects/Simon game/app.js
+++ Projects/Simon game/app.js
@@ -4,6 +4,7 @@ let btns= ["yellow", "red", "purple", "green"];
 let started= false;// intially game is not started 
 let level=0;// intial level is 0
 
+
 let h2= document.querySelector("h2");
 
 document.addEventListener("keypress", function(){
