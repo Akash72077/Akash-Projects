38571bc27522338ed9784e099fd4b106c73f5224
Akash <dudigamvenkatasaiakash@gmail.com>
2026-06-23 23:46:08 +0530

    fixed typo

--- DSA/BinaryTree/Apna.java
+++ DSA/BinaryTree/Apna.java
@@ -2,7 +2,7 @@ public class Apna {
     public static void main(String[] args) {
         // -1 indicates null
         // building tree in preorder traversal
-        // pre order traversal means : first root node , next left node and righ tnode 
+        // pre order traversal means : first root node , next left node and right node 
         int nodes[]= {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
     }
 }
