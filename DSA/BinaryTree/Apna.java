05a0d8d571888e46cf59eecb48b4be5afb403b5f
Akash <dudigamvenkatasaiakash@gmail.com>
2026-06-23 23:45:23 +0530

    cmt

--- DSA/BinaryTree/Apna.java
+++ DSA/BinaryTree/Apna.java
@@ -1,6 +1,7 @@
 public class Apna {
     public static void main(String[] args) {
         // -1 indicates null
+        // building tree in preorder traversal
         int nodes[]= {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
     }
 }
