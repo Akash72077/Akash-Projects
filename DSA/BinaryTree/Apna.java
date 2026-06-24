4e31deed2b7636ec50c69f81583e942272722b42
Akash <sai.sai72077@gmail.com>
2026-06-24 23:52:21 +0530

    Clarify comment for nodes array in preorder traversal
    Updated comment for clarity on the nodes array used for tree construction.

--- DSA/BinaryTree/Apna.java
+++ DSA/BinaryTree/Apna.java
@@ -29,6 +29,6 @@ public class Apna {
         // -1 indicates null
         // building tree in preorder traversal
         // pre order traversal means : first root node , next left node and right node 
-        int nodes[]= {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        int nodes[]= {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1}; // array for preorder traversal
     }
 }
