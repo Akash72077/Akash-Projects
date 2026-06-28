96790b184aabc3d006ac6ad12512a41760297ed9
Akash <dudigamvenkatasaiakash@gmail.com>
2026-06-28 19:05:06 +0530

    writing java

--- JavaDev/TodoApp/src/main/java/com/app/TodoApp/controller/TaskController.java
+++ JavaDev/TodoApp/src/main/java/com/app/TodoApp/controller/TaskController.java
@@ -1,15 +1,18 @@
 package com.app.TodoApp.controller;
 
-import com.app.TodoApp.services.TaskService;
 import com.app.TodoApp.models.Task;
+import com.app.TodoApp.services.TaskService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.List;
 
 @Controller
-//@RequestMapping("/tasks")
+@RequestMapping("/tasks")
 public class TaskController {
 
     private final TaskService taskService;
@@ -19,10 +22,25 @@ public class TaskController {
     }
 
     @GetMapping
-        public  String getTask(Model model){
-        List<Task> tasks= taskService.getAllTasks();
-        model.addAttribute("tasks",tasks);
-        return "tasks";
+    public String getTask(Model model) {
+
+        List<Task> tasks = taskService.getAllTasks();
+
+        System.out.println("Size = " + tasks.size());
+
+        for (Task task : tasks) {
+            System.out.println(task.getTitle());
         }
 
+        model.addAttribute("tasks", tasks);
+
+        return "tasks";
+    }
+    @PostMapping
+    public String createTask(@RequestParam String title) {
+
+        taskService.createTask(title);
+
+        return "redirect:/";
+    }
 }
