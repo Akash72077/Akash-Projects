ef2e4f04d40c19dca15c2c9c6f7219e431526958
Akash <dudigamvenkatasaiakash@gmail.com>
2026-06-29 15:47:51 +0530

    Code completed for printing numbers from n to 1

--- DSA/Recursion/com/Learn/NToOne.java
+++ DSA/Recursion/com/Learn/NToOne.java
@@ -1,11 +1,23 @@
 package com.Learn;
 
+import java.util.Scanner;
+
 public class NToOne {
     static void printDecreasing(int n){
+        if(n==1){
+            System.out.println(n);
+            return;
+        }
+        System.out.println(n);
+        printDecreasing(n-1);
 
     }
     public static void main(String[] args) {
-
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter N value to print from N to 1: ");
+        int n=sc.nextInt();
+        System.out.println("The values from "+n+" to 1 are: ");
+        printDecreasing(n);
 
     }
 
