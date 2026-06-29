b1faa634a49d54964c13f3c3d34a5ab71656eb6f
Akash <dudigamvenkatasaiakash@gmail.com>
2026-06-29 12:27:34 +0530

    Code written for fibonaccu

--- DSA/Recursion/com/Learn/Fibonacci.java
+++ DSA/Recursion/com/Learn/Fibonacci.java
@@ -1,7 +1,18 @@
 package com.Learn;
 
 public class Fibonacci {
+
+    static int fibo(int n){
+
+        if(n<2)
+            return n;
+
+        return fibo(n-1)+fibo(n-2);
+    }
+
+
     public static void main(String[] args) {
 
+        System.out.println(fibo(6));
     }
 }
