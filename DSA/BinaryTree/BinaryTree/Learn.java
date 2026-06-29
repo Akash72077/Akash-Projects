ea5e7a8beafbdcb1da4660103edf0567a8281500
Akash <dudigamvenkatasaiakash@gmail.com>
2026-06-30 00:46:42 +0530

    Inserted values

--- DSA/BinaryTree/BinaryTree/Learn.java
+++ DSA/BinaryTree/BinaryTree/Learn.java
@@ -2,20 +2,28 @@ package BinaryTree;
 
 public class Learn
 {
-
    public static class Node{
         int val;
         Node left;
         Node right;
+       public Node(int val){
+            this.val= val;
+        }
     }
     public static void main(String[] args) {
-        Node root = new Node();
-        root.val=10;
-        root.left=null;
-        root.right=null;
-        System.out.println(root.val);
-        System.out.println(root.left);
-        System.out.println(root.right);
+        Node root = new Node(1);
+        Node a = new Node(2);
+        Node b = new Node(3);
+        Node c = new Node(4);
+        Node d = new Node(5);
+        Node e = new Node(6);
+        root.left=a;
+        root.right=b;
+        a.left=c;
+        a.right=d;
+        b.right=e;
 
+
+        System.out.println(root.val);
     }
 }
