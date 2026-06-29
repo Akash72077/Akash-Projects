91e4158986b8a1ed4ee7b9561c59a0b3eebb09ab
Akash <dudigamvenkatasaiakash@gmail.com>
2026-06-30 00:35:58 +0530

    written broiler plate code for tree

--- DSA/BinaryTree/BinaryTree/Apna.java
+++ DSA/BinaryTree/BinaryTree/Apna.java
@@ -1,7 +1,7 @@
 package BinaryTree;
 
 public class Apna {
-    static class Node{// Class for Node 
+    static class Node{// Class for Node
         int data; //it contains data part
         Node left;
         Node right;
@@ -15,29 +15,26 @@ public class Apna {
 
     static class BinaryTree {
         static int index=-1;
-        public static Node buildTree(int nodes[]){  // here data type Node means it will return root
+        public static Node buildTree(int[] nodes){  // here data type Node means it will return root
             index++;
         if(nodes[index]==-1){
-            return null;// returns ie. stops running if it contains -1
+            return null;// returns ie stops running if it contains -1
         }
         Node newNode= new Node(nodes[index]);
         newNode.left=buildTree(nodes);
         newNode.right= buildTree(nodes);
         return newNode;
         }
-        
+
     }
     public static void main(String[] args) {
         // -1 indicates null
         // building tree in preorder traversal
-        // pre order traversal means : first root node , next left node and right node 
-<<<<<<< HEAD
-        int nodes[]= {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
+        // Pre Order traversal means : first root node , next left node and right node
+        int[] nodes = {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1};
         BinaryTree tree= new BinaryTree();
         Node root= tree.buildTree(nodes);
         System.out.println(root.data);
-=======
-        int nodes[]= {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1}; // array for preorder traversal
->>>>>>> 4e31deed2b7636ec50c69f81583e942272722b42
+//        int nodes[]= {1,2,4,-1,-1,5,-1,-1,3,-1,6,-1,-1}; // array for preorder traversal
     }
 }
--- DSA/BinaryTree/BinaryTree/Learn.java
+++ DSA/BinaryTree/BinaryTree/Learn.java
@@ -3,12 +3,19 @@ package BinaryTree;
 public class Learn
 {
 
-    class Node{
+   public static class Node{
         int val;
         Node left;
         Node right;
     }
     public static void main(String[] args) {
+        Node root = new Node();
+        root.val=10;
+        root.left=null;
+        root.right=null;
+        System.out.println(root.val);
+        System.out.println(root.left);
+        System.out.println(root.right);
 
     }
 }
