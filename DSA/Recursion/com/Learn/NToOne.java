c6fb05265c9c0a5fbf0502e5cb8df27e2be6ed98
Akash <dudigamvenkatasaiakash@gmail.com>
2026-06-29 16:13:11 +0530

    code written for 1 to n

--- DSA/Recursion/com/Learn/NToOne.java
+++ DSA/Recursion/com/Learn/NToOne.java
@@ -21,4 +21,21 @@ public class NToOne {
 
     }
 
+    public static class OneToN {
+        static void printIncreasing(int n){
+            if(n==1){
+                System.out.println(n);
+                return;
+            }
+            printIncreasing(n-1);
+            System.out.println(n);
+
+        }
+           public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            int n=sc.nextInt();
+            printIncreasing(n);
+            sc.close();
+        }
+    }
 }
---
+++ DSA/Recursion/com/Learn/OneToN.java
@@ -0,0 +1,23 @@
+package com.Learn;
+
+import java.util.Scanner;
+
+
+    public class OneToN {
+        static void printIncreasing(int n){
+            if(n==1){
+                System.out.println(n);
+                return;
+            }
+            printIncreasing(n-1);
+            System.out.println(n);
+
+        }
+        public static void main(String[] args) {
+            Scanner sc=new Scanner(System.in);
+            int n=sc.nextInt();
+            printIncreasing(n);
+            sc.close();
+        }
+    }
+
