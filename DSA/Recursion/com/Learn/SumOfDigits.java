b99a84fe29d216d7b555b96394674eec51716760
Akash <dudigamvenkatasaiakash@gmail.com>
2026-06-29 21:36:41 +0530

    code written for leetcode probel

--- DSA/Recursion/com/Learn/SumOfDigits.java
+++ DSA/Recursion/com/Learn/SumOfDigits.java
@@ -2,7 +2,16 @@ package com.Learn;
 
 public class SumOfDigits
 {
+    public static int sum ( int n){
+        if (n == 0)
+            return 0;
+        return (n % 10) + sum(n / 10);
+    }
     public static void main(String[] args) {
 
+            int n = 1432;
+            System.out.println(sum(n));
+
+
+        }
     }
-}
