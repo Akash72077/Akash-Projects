e5ab585a5f95541ae4d71842325800f245fb5d60
Akash <dudigamvenkatasaiakash@gmail.com>
2026-06-30 23:27:40 +0530

    taken input

--- DSA/Stacks/BuildAnArrayWithStackOperations.java
+++ DSA/Stacks/BuildAnArrayWithStackOperations.java
@@ -12,8 +12,14 @@ public class BuildAnArrayWithStackOperations {
     public static void main(String[] args) {
             Scanner sc= new Scanner(System.in);
         System.out.println("Enter size of an array: ");
+        int size=sc.nextInt();
+        int[] arr= new int[size];
+        System.out.println("Enter "+size+" elements to array: ");
+        for (int i = 0; i < size; i++) {
+            arr[i]=sc.nextInt();
+        }
+        System.out.println("Enter value of n: ");
         int n=sc.nextInt();
-        int[] arr= new int[n];
 
     }
 }
