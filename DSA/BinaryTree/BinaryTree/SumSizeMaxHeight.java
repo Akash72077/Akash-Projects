d7917e26e15fc3396d9c78980121e6df88a2c352
Akash <dudigamvenkatasaiakash@gmail.com>
2026-07-01 23:41:51 +0530

    Nodes added

--- DSA/BinaryTree/BinaryTree/SumSizeMaxHeight.java
+++ DSA/BinaryTree/BinaryTree/SumSizeMaxHeight.java
@@ -2,7 +2,30 @@ package BinaryTree;
 
 public class SumSizeMaxHeight
 {
-    public static void main(String[] args) {
+    static class Node{
+        int val;
+        Node left;
+        Node right;
+        Node(int val){
+            this.val=val;
+        }
 
     }
+    public static void main(String[] args) {
+        Node a= new Node(1);
+        Node b= new Node(2);
+        Node c= new Node(3);
+        Node d= new Node(4);
+        Node e= new Node(5);
+        Node f= new Node(6);
+        Node g= new Node(7);
+        a.left=b;
+        a.right=c;
+        b.left=d;
+        b.right=e;
+        c.left=f;
+        c.right=g;
+
+
+        }
 }
