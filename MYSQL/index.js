74e4bce823e397c8d5600cdf6192240390ac29f1
Akash <sai.sai72077@gmail.com>
2026-07-02 23:56:07 +0530

    Fix comment formatting in database connection

--- MYSQL/index.js
+++ MYSQL/index.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 // Create the connection to database
 
 const connection = mysql.createConnection({
-  host: 'localhost',// connecting with db
+  host: 'localhost',// connecting with database
   user: 'root',
   database: 'akash',
   password:  'Akash@12345'
