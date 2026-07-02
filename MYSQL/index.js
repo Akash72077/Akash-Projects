6bc14cf25db964bad3c6ea27068afb3023859bf8
Akash <sai.sai72077@gmail.com>
2026-07-02 23:55:25 +0530

    Clarify host property in database connection
    Added comment to clarify the purpose of the host property.

--- MYSQL/index.js
+++ MYSQL/index.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 // Create the connection to database
 
 const connection = mysql.createConnection({
-  host: 'localhost',
+  host: 'localhost',// connectinf with db
   user: 'root',
   database: 'akash',
   password:  'Akash@12345'
