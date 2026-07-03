00c048fad94b97cb917b0759770881f18705ebd2
Akash <dudigamvenkatasaiakash@gmail.com>
2026-07-03 23:49:32 +0530

    new class created

---
+++ DSA/Stacks/Problems/CopyStackInSameOrder.java
@@ -0,0 +1,9 @@
+package Problems;
+
+import java.util.Stack;
+
+public class CopyStackInSameOrder {
+    public static void main(String[] args) {
+        Stack<Integer> st= new Stack<>();
+    }
+}
