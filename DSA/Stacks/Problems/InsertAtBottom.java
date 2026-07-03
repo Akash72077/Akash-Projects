d53656ff32365ee4bac0fc2277e2b87650bd465c
Akash <dudigamvenkatasaiakash@gmail.com>
2026-07-04 00:14:06 +0530

    code written  for insert at bottom

--- DSA/Stacks/Problems/InsertAtBottom.java
+++ DSA/Stacks/Problems/InsertAtBottom.java
@@ -1,4 +1,28 @@
 package Problems;
 
+import java.util.Scanner;
+import java.util.Stack;
+
 public class InsertAtBottom {
+    public static void main(String[] args) {
+        Stack<Integer>st= new Stack<>();
+        Stack<Integer>st1= new Stack<>();
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter size of stack: ");
+        int n=sc.nextInt();
+        System.out.println("Enter "+n+" Elements to Stack: ");
+        for (int i = 0; i < n; i++) {
+            st.push(sc.nextInt());
+        }
+        System.out.println("Enter the element which you want insert at bottom");
+        int element= sc.nextInt();
+        while(!st.isEmpty()){
+            st1.push(st.pop());
+        }
+        st.push(element);
+        while(!st1.isEmpty()){
+            st.push(st1.pop());
+        }
+        System.out.println(st);
+    }
 }
