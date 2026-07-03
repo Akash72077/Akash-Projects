5a0613384b083035c99d76ed2227d77ed4c12bee
Akash <dudigamvenkatasaiakash@gmail.com>
2026-07-03 23:59:00 +0530

    Code written to copy element to another stack with same order

--- DSA/Stacks/Problems/CopyStackInSameOrder.java
+++ DSA/Stacks/Problems/CopyStackInSameOrder.java
@@ -1,9 +1,30 @@
 package Problems;
 
+import java.util.Scanner;
 import java.util.Stack;
 
 public class CopyStackInSameOrder {
     public static void main(String[] args) {
         Stack<Integer> st= new Stack<>();
+        Stack<Integer> st1= new Stack<>();
+        Stack<Integer> st2= new Stack<>();
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter number of elements you want to insert: ");
+        int n= sc.nextInt();
+        int i=0;
+        System.out.println("Enter "+n+" Elements to Stack: ");
+        while(i<n){
+            st.push(sc.nextInt());
+            i++;
+        }
+        while(!st.isEmpty()){
+            st1.push(st.peek());
+            st.pop();
+        }
+        while(!st1.isEmpty()){
+            st2.push(st1.peek());
+            st1.pop();
+        }
+        System.out.println(st2);
     }
 }
