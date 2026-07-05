a215f2d16e9559d1f25f7c371864e867712067f0
Akash <dudigamvenkatasaiakash@gmail.com>
2026-07-05 16:23:54 +0530

    Code written for implementation of stack using array

--- DSA/Stacks/Learn/StackUsingArray.java
+++ DSA/Stacks/Learn/StackUsingArray.java
@@ -1,4 +1,51 @@
 package Learn;
 
 public class StackUsingArray {
+public static  class Stack {
+
+    int[] arr = new int[10];
+    int top = -1;
+
+    void push(int element) {
+        if(top==arr.length){
+            System.out.println("Stack overflow");
+            return;
+        }
+        arr[++top] = element;
+    }
+
+    int pop() {
+        if(top==-1){
+            System.out.println("Stack is empty");
+            return -1;
+        }
+        return arr[top--];
+
+    }
+
+    int peek() {
+        return arr[top];
+    }
+
+    void display() {
+        int i = 0;
+        while (i <= top) {
+            System.out.print(arr[i] + " ");
+            i++;
+        }
+    }
+}
+
+    public static void main(String[] args) {
+        Stack st= new Stack();
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+        st.push(5);
+        System.out.println(st.pop());
+        System.out.println(st.peek());
+
+        st.display();
+    }
 }
