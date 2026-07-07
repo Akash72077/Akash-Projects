213b789b5c504dc28fb2f202cb836e586b69ea6e
Akash <dudigamvenkatasaiakash@gmail.com>
2026-07-07 20:15:29 +0530

    Code written for valid  paranthesis

--- DSA/Stacks/Problems/BalancedBrackets.java
+++ DSA/Stacks/Problems/BalancedBrackets.java
@@ -4,8 +4,33 @@ import java.util.Stack;
 
 public class BalancedBrackets
 {
+    public  static  boolean Valid(String str){
+        int len=str.length();
+        if(len%2!=0){
+            return  false;
+        }
+        Stack<Character>st= new Stack<>();
+        for(int i=0; i<len; i++){
+            if(str.charAt(i)=='{'||str.charAt(i)=='('||str.charAt(i)=='['){
+                st.push(str.charAt(i));
+            }else{
+                if(st.isEmpty()){
+                    return false;
+                } else if (str.charAt(i)=='}' && st.peek()=='{'||str.charAt(i)==')' && st.peek()=='('||str.charAt(i)==']' && st.peek()=='[') {
+                st.pop();
+                }
+                else {
+                    return false;
+                }
+            }
+        }
+        return st.isEmpty();
+
+    }
     public static void main(String[] args) {
-        Stack<Integer>st= new Stack<>();
+
+        String str= "({}){[[]]()}";
+        System.out.println(Valid(str));
 
     }
 }
