99bdb36dd32b67880dc4a653be1af9299d53bc8b
Akash <dudigamvenkatasaiakash@gmail.com>
2026-07-07 23:40:30 +0530

    File created and written main function for remove consecutive sequence

---
+++ DSA/Stacks/Problems/RemoveConsecutiveSubsequence.java
@@ -0,0 +1,13 @@
+package Problems;
+//Q. Remove consecutive subsequences
+//Given a sequence of numbers. Remove all the
+//consecutive subsequences of length greater than or
+//equal to 2 that contains the same element.
+import Learn.StackUsingArray;
+
+public class RemoveConsecutiveSubsequence
+{
+    public static void main(String[] args) {
+
+    }
+}
