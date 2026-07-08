587b0c01e595433f6bf1a000cbdb08b76270517d
Akash <dudigamvenkatasaiakash@gmail.com>
2026-07-08 13:52:50 +0530

    function written for next greater element bruteforce

--- DSA/Stacks/Problems/NextGreaterElement.java
+++ DSA/Stacks/Problems/NextGreaterElement.java
@@ -1,6 +1,26 @@
 package Problems;
 
 public class NextGreaterElement {
+    public static  int[] nextGreaterElement(int[] arr){
+        int n= arr.length;
+        int[] res= new int[n];
+        for(int i=0; i<n; i++){
+            res[i]=-1;
+            for(int j=i+1; j<n; j++ ){
+                if(arr[i]<arr[j]){
+                    res[i]=arr[j];
+                    break;
+                }
+            }
+        }
+        return res;
+    }
+    public static  int[] nextGreaterElementLeetcode(int[] nums1, int[] nums2) {
+        int n=nums1.length;
+        int[] res= new int[n];
+
+        return res;
+    }
     public static void main(String[] args) {
 
         for (int i = 0; i < n; i++) {
