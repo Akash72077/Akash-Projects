d0fa901b9846eaafb56152da87f7ba65bd7a727f
Akash <dudigamvenkatasaiakash@gmail.com>
2026-07-08 13:52:04 +0530

    for loop written

--- DSA/Stacks/Problems/NextGreaterElement.java
+++ DSA/Stacks/Problems/NextGreaterElement.java
@@ -3,5 +3,8 @@ package Problems;
 public class NextGreaterElement {
     public static void main(String[] args) {
 
+        for (int i = 0; i < n; i++) {
+            System.out.print(res[i]+" ");
+        }
     }
 }
