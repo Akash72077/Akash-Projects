7466c9067eb63c99fc0257aaf7c2667c04548273
Akash <dudigamvenkatasaiakash@gmail.com>
2026-07-11 22:58:29 +0530

    Code written for Next greater element with stacks

--- DSA/Stacks/Problems/NextGreaterElement.java
+++ DSA/Stacks/Problems/NextGreaterElement.java
@@ -1,7 +1,31 @@
 package Problems;
 import java.util.Scanner;// importing Scanner class
+import java.util.Stack;
 
 public class NextGreaterElement {
+
+    public static int[] nextGreaterStack(int[] arr){
+        Stack<Integer> st= new Stack<>();
+        int n= arr.length;
+        int[] res= new int[n];
+
+            st.push(arr[n-1]);
+        res[n-1]=-1;
+        for(int i=n-2; i>=0; i--){
+            while( !st.isEmpty() && st.peek()<arr[i] ){
+                st.pop();
+            }
+            if(st.isEmpty()){
+                res[i]=-1;
+            }else{
+                res[i]=st.peek();
+            }
+            st.push(arr[i]);
+        }
+
+        return  res;
+
+    }
     public static  int[] nextGreaterElement(int[] arr){
         int n= arr.length;
         int[] res= new int[n];
@@ -45,31 +69,34 @@ public class NextGreaterElement {
     }
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-//    int[] arr={1,3,2,1,8,6,3,4};
-//    int n= arr.length;
+    int[] arr={1,3,2,1,8,6,3,4};
+    int n= arr.length;
 //   int[] res= nextGreaterElement(arr);
 //        for (int i= 0; i< n; i++) {
 //            System.out.print(res[i]+" ");
 //        }
-        System.out.println("Enter size of nums1: ");
-        int m= sc.nextInt();
-        int[] nums1= new int[m];
-        System.out.println("Enter size of nums2: ");
-        int n= sc.nextInt();
-        int[] nums2= new int[n];
-        System.out.println("Enter elements into nums1: ");
-        for (int i = 0; i < m; i++) {
-            nums1[i]= sc.nextInt();
-        }
-        System.out.println("Enter elements into nums2: ");
-        for (int i = 0; i < n; i++) {
-            nums2[i]= sc.nextInt();
-        }
-        int[] ans=nextGreaterElementLeetcode(nums1, nums2);
-                for (int i = 0; i < m; i++) {
-            System.out.print(ans[i]+" ");
-        }
-
+//        System.out.println("Enter size of nums1: ");
+//        int m= sc.nextInt();
+//        int[] nums1= new int[m];
+//        System.out.println("Enter size of nums2: ");
+//        int n= sc.nextInt();
+//        int[] nums2= new int[n];
+//        System.out.println("Enter elements into nums1: ");
+//        for (int i = 0; i < m; i++) {
+//            nums1[i]= sc.nextInt();
+//        }
+//        System.out.println("Enter elements into nums2: ");
+//        for (int i = 0; i < n; i++) {
+//            nums2[i]= sc.nextInt();
+//        }
+//        int[] ans=nextGreaterElementLeetcode(nums1, nums2);
+//                for (int i = 0; i < m; i++) {
+//            System.out.print(ans[i]+" ");
+//        }
+            int[] res=nextGreaterStack(arr);
+            for(int i=0; i<n; i++){
+                System.out.print(res[i]+" ");
+            }
         sc.close();
     }
 }
