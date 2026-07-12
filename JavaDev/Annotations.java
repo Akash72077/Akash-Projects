ac7541c1fe979be100aa0ae523cb0346ebdf1303
Akash <dudigamvenkatasaiakash@gmail.com>
2026-07-12 16:58:15 +0530

    Written example for annotation with defination

--- JavaDev/Annotations.java
+++ JavaDev/Annotations.java
@@ -1,6 +1,28 @@
-public class annotations {
+// prereqisites are interfaces and basic java
+/* Defination:
+                An annotations is a piece of metadata(information about your code)
+        It gives instructions to:
+            *)The compiler
+            *)The JVM
+            *) or frame works like (Spring , Hibernate)
+            
+        Annotations do not directly change the program logic. They provide extra information that tools or frame works can use.
 
+    Syntax:
+        Annotations starts with '@'
+
+    Example:
+    
+    @Override
+    public void display() {
+
+    }
+
+*/  
+public class annotations {
     public static void main(String[] args) {
+
+
         
     }
 }
