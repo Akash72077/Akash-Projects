e65cf577e236571c79c0de9e3cdbff5ae2175e24
Akash <dudigamvenkatasaiakash@gmail.com>
2026-07-15 20:22:53 +0530

    Brute force code written

--- DSA/Stacks/Problems/DailyTemperatures.java
+++ DSA/Stacks/Problems/DailyTemperatures.java
@@ -1,17 +1,28 @@
 package Problems;
 public class DailyTemperatures {
-    public  static  int [] BruteForce(int arr[]){
+    public  static  int [] BruteForce(int[] arr){
+            int n=arr.length;
+        int[] res =new int[arr.length];
+        for(int i=0;i<n-1;i++){
+            for(int j=i+1;j<n;j++){
+                if(arr[i]<arr[j]){
+                    res[i]=j-i;
+                    break;
+                }
+            }
+        }
 
-        int res[]=new int[arr.length];
-
-
-
+    return res;
     }
 
 
     public static void main(String[] args) {
 
-
+        int[] arr= {73,74,75,71,69,72,76,73};
+       int[] res= BruteForce(arr);
+        for (int re : res) {
+            System.out.print(re + " ");
+        }
 
     }
 }
