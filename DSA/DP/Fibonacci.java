1fc51ddb6686f49ad23060ec1b3de8808881c383
Akash <dudigamvenkatasaiakash@gmail.com>
2026-07-16 23:35:35 +0530

    Comment line written

--- DSA/DP/Fibonacci.java
+++ DSA/DP/Fibonacci.java
@@ -1,6 +1,9 @@
 public class Fibonacci {
 
    static int fib(int n) {
+
+       // here we used recursion but the disadvantage is the time complexity is 2^n because in recursion tree the new functions calls will happen for already calculated value
+
         if (n == 0 || n == 1)
             return n;
         else
