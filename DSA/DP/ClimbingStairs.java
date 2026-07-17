e45d59e9ca2e72fc8a49375edf99ad4aed7a1916
Akash <dudigamvenkatasaiakash@gmail.com>
2026-07-17 23:06:22 +0530

    Code written for climbing stairs using tabulation method

--- DSA/DP/ClimbingStairs.java
+++ DSA/DP/ClimbingStairs.java
@@ -1,4 +1,29 @@
-package PACKAGE_NAME;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 
 public class ClimbingStairs {
+
+
+        static int climbStairsTabulation(int n) {
+            if(n==1)
+                return 1;
+            int[] dp = new int[n+1];
+            dp[1]=1;
+            dp[2]=2;
+            for(int i=3; i<=n; i++){
+                dp[i]=dp[i-1]+dp[i-2];
+            }
+            return dp[n];
+        }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of stairs: ");
+        int n=sc.nextInt();
+        System.out.println(climbStairsTabulation(n));
+
+
+    }
 }
