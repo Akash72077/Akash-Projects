5c6dcbc6f9b6121fa3bfdcceb295852d1920c079
Akash <dudigamvenkatasaiakash@gmail.com>
2026-08-02 12:36:15 +0530

    converted to post

--- Eclipse/ServletGetPost/src/main/java/ServletApp.java
+++ Eclipse/ServletGetPost/src/main/java/ServletApp.java
@@ -13,7 +13,7 @@ public class ServletApp extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
 	
-	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
 	System.out.println("Control in servlet/controller");	
 			
