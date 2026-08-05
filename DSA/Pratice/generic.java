cd0a6dd251e1c8bfcd2ca94a4d73f446145401f7
Akash <dudigamvenkatasaiakash@gmail.com>
2026-08-05 15:32:36 +0530

    code written using generic classes

--- DSA/Pratice/generic.java
+++ DSA/Pratice/generic.java
@@ -4,8 +4,20 @@
 // body of the method
 //}
 
+import java.util.Arrays;
+
 public class generic {
+
+   static <T> void print(T t){
+        System.out.println(t);
+    }
+
     public static void main(String[] args) {
-        
+        generic obj=new generic();
+        obj.print(10);
+        obj.print(null);
+        obj.print("sai akash");
+        int[] arr={1,2,3,4,5,6};
+        obj.print(Arrays.toString(arr));
     }
 }
