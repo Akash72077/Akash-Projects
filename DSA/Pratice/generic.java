124dcaf25ec4e8065429460c064af3c516ccefc5
Akash <dudigamvenkatasaiakash@gmail.com>
2026-08-05 15:20:31 +0530

    written defination

---
+++ DSA/Pratice/generic.java
@@ -0,0 +1,11 @@
+// a method that can refer to any data type is called generic data type 
+
+// syntax: <access specifier> <return type> mName(<type list>){
+// body of the method
+//}
+
+public class generic {
+    public static void main(String[] args) {
+        
+    }
+}
