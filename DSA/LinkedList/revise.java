e5d3b3906eb98dff10a394031aa2505be44fc72a
Akash <dudigamvenkatasaiakash@gmail.com>
2026-08-07 22:01:31 +0530

    reverse file changed

--- DSA/LinkedList/revise.java
+++ DSA/LinkedList/revise.java
@@ -49,7 +49,7 @@ public class revise {
         Node fast= head;
         while(fast != null && fast.next != null){
             slow=slow.next;
-        fast=fast.next.next;
+             fast=fast.next.next;
         }
         return slow;
     }
