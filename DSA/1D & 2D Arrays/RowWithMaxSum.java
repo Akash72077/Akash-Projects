2968d97a5cbfaf2e6b57ccaa6ea383f41a50b526
Akash <dudigamvenkatasaiakash@gmail.com>
2026-08-09 12:32:05 +0530

    Code written for row with max sum in 2d arrays

--- DSA/1D & 2D Arrays/RowWithMaxSum.java
+++ DSA/1D & 2D Arrays/RowWithMaxSum.java
@@ -1,2 +1,46 @@
+import java.util.Scanner;
 public class RowWithMaxSum {
+    static void display(int []arr){
+        System.out.println("The maximum value: "+arr[1]+" in row "+arr[0]);
+    }
+
+    static int[] maxRow(int [][]arr){
+        int n=arr.length;
+        if(n == 0){
+            return new int[0];
+        }
+
+        int max=0;
+        int row=0;
+        for(int i=0; i<n; i++){
+            int sum=0;
+            for(int j=0; j<arr[i].length; j++){
+                sum+=arr[i][j];
+            }
+            if(sum>max){
+                max=sum;
+                row=i;
+            }
+        }
+        return new int[]{row , max};
+    }
+
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter no of rows & cols of matrix: ");
+        int n=sc.nextInt();
+        int m=sc.nextInt();
+        int [][]arr= new int[n][m];
+        System.out.println("Enter elements: ");
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                arr[i][j]= sc.nextInt();
+            }
+        }
+        int [] sol=maxRow(arr);
+        display(sol);
+
+
+    }
 }
